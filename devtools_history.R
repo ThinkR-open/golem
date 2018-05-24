2c67b396098ac33ffda27bc24589fe05f0f9bd03
Colin Fay <colin@thinkr.fr>
2018-05-24 11:47:31 +0200

    project update

--- devtools_history.R
+++ devtools_history.R
@@ -1,2 +1,9 @@
+library(usethis)
+
 devtools::use_package("devtools")
 usethis::use_build_ignore("devtools_history.R")
+usethis::use_build_ignore("readme_figures/")
+usethis::use_readme_rmd()
+usethis::use_mit_license(name = "ThinkR")
+use_code_of_conduct()
+use_lifecycle_badge("Experimental")
