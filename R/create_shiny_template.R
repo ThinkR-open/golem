8103b8abf674e57b023dda457c779a50fa817abe
Vincent <vincent@thinkr.fr>
2018-05-24 13:03:15 +0200

    # remove `..`

--- R/create_shiny_template.R
+++ R/create_shiny_template.R
@@ -5,9 +5,11 @@
 #'
 #' @export
 create_shiny_template <- function(path, ...) {
-dir.create(path, recursive = TRUE, showWarnings = FALSE)
-devtools::create(path = path)
-from <- system.file("shinytemplate","",package = "shinytemplate")
-ll <- list.files(path = from, full.names = TRUE, all.files = TRUE)
-file.copy(from = ll, to = path, overwrite = TRUE, recursive = TRUE)
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  devtools::create(path = path)
+  from <- system.file("shinytemplate",package = "shinytemplate")
+  ll <- list.files(path = from, full.names = TRUE, all.files = TRUE)
+  # remove `..`
+  ll <- ll[ ! ll %>% stringr::str_detect("\\.\\.$")]
+  file.copy(from = ll, to = path, overwrite = TRUE, recursive = TRUE)
 }
