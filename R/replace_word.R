183f4d2b7dfdbff78ad2d85d2c61e59ae6478dc8
Vincent <vincent@thinkr.fr>
2018-06-04 11:23:22 +0200

    replace -> replacement in gsub

--- R/replace_word.R
+++ R/replace_word.R
@@ -1,5 +1,5 @@
 replace_word <- function(file,pattern, replace){
 suppressWarnings( tx  <- readLines(file) )
-  tx2  <- gsub(pattern = pattern, replace = replace, x = tx)
+  tx2  <- gsub(pattern = pattern, replacement = replace, x = tx)
   writeLines(tx2, con=file)
 }
