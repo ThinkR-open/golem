2d0cdfbc3701803fa56073f3dabfb2c1d2578ac1
Colin <colin@thinkr.fr>
2019-01-24 22:39:01 +0100

    created use_recommended_tests

--- R/use_recommended.R
+++ R/use_recommended.R
@@ -13,3 +13,17 @@ use_recommended_dep <- function(pkg = "."){
   usethis::use_package("attempt")
   usethis::use_package("glue")
 }
+
+use_recommended_tests <- function(pkg = "."){
+  old <- setwd(normalizePath(pkg))
+  on.exit(setwd(old))
+  if (!dir.exists(
+    file.path(normalizePath(pkg), "tests")
+  )){
+    usethis::use_testthat()
+  }
+  file.copy(
+    system.file("utils", "test-golem-recommended.R", package = "golem"), 
+    file.path(normalizePath(pkg), "tests", "testthat")
+  )
+} 
