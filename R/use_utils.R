5eb4fd0067e31dc9b83f01582d7a4b2a20639350
Vincent Guyader <vincent@thinkr.fr>
2019-02-02 11:43:49 +0100

    use_utils refactoring

--- R/use_utils.R
+++ R/use_utils.R
@@ -5,37 +5,32 @@
 #' @export
 #' @rdname utils_files
 #' @importFrom cli cat_bullet
+#' @importFrom glue glue
 #' 
 use_utils_ui <- function(pkg = "."){
-  old <- setwd(normalizePath(pkg))
-  on.exit(setwd(old))
-  file.copy(
-    system.file("utils", "utils_ui.R", package = "golem"), 
-    file.path(normalizePath(pkg), "R", "utils_ui.R")
-  )
-  cat_bullet("File added", bullet = "tick", bullet_col = "green")
+  use_utils(file_name = "utils_ui.R", pgk=pkg)
 }
 
 #' @export
 #' @rdname utils_files
 use_utils_prod <- function(pkg = "."){
-  old <- setwd(normalizePath(pkg))
-  on.exit(setwd(old))
-  file.copy(
-    system.file("utils", "utils_prod.R", package = "golem"), 
-    file.path(normalizePath(pkg), "R", "utils_prod.R")
-  )
-  cat_bullet("File added", bullet = "tick", bullet_col = "green")
+  use_utils(file_name = "utils_prod.R", pgk=pkg)
 }
 
 #' @export
 #' @rdname utils_files
 use_utils_server <- function(pkg = "."){
+  use_utils(file_name = "utils_server.R", pgk=pkg)
+} 
+
+use_utils <- function(file_name,pkg = "."){
   old <- setwd(normalizePath(pkg))
   on.exit(setwd(old))
   file.copy(
-    system.file("utils", "utils_server.R", package = "golem"), 
-    file.path(normalizePath(pkg), "R", "utils_server.R")
+    system.file("utils", file_name, package = "golem"), 
+    to <- file.path(normalizePath(pkg), "R", file_name)
   )
-  cat_bullet("File added", bullet = "tick", bullet_col = "green")
+  cat_bullet(glue::glue("File created at {to}"), bullet = "tick", bullet_col = "green")
 } 
+
+"utils_server.R"
