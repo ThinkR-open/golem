2e48f72c337957e78775f7548d73a6f12992faaf
Colin <colin@thinkr.fr>
2019-02-03 22:32:56 +0100

    better doc for use_utils

--- R/use_utils.R
+++ R/use_utils.R
@@ -1,26 +1,32 @@
 #' Use the utils files
+#' 
+#' \describe{
+#'   \item{use_utils_ui}{Copies the golem_utils_ui.R to the R folder.}
+#'   \item{use_utils_prod}{Copies the golem_utils_prod.R to the R folder.}
+#'   \item{use_utils_server}{Copies the golem_utils_server.R to the R folder.}
+#' }
 #'
 #' @inheritParams add_module
 #'
 #' @export
 #' @rdname utils_files
+#' 
 #' @importFrom cli cat_bullet
 #' @importFrom glue glue
-#' 
 use_utils_ui <- function(pkg = "."){
-  use_utils(file_name = "utils_ui.R", pkg=pkg)
+  use_utils(file_name = "golem_utils_ui.R", pkg=pkg)
 }
 
 #' @export
 #' @rdname utils_files
 use_utils_prod <- function(pkg = "."){
-  use_utils(file_name = "utils_prod.R", pkg=pkg)
+  use_utils(file_name = "golem_utils_prod.R", pkg=pkg)
 }
 
 #' @export
 #' @rdname utils_files
 use_utils_server <- function(pkg = "."){
-  use_utils(file_name = "utils_server.R", pkg=pkg)
+  use_utils(file_name = "golem_utils_server.R", pkg=pkg)
 } 
 
 use_utils <- function(file_name,pkg = "."){
@@ -33,4 +39,4 @@ use_utils <- function(file_name,pkg = "."){
   cat_bullet(glue::glue("File created at {to}"), bullet = "tick", bullet_col = "green")
 } 
 
-"utils_server.R"
+# "utils_server.R"
