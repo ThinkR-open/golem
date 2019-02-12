66ce91850fc67d1c41d47c0e15f483603711bab7
Vincent Guyader <vincent@thinkr.fr>
2019-02-12 18:06:29 +0100

    Imports: shiny

--- inst/shinyexample/R/app_server.R
+++ inst/shinyexample/R/app_server.R
@@ -1,3 +1,4 @@
+#' @import shiny
 app_server <- function(input, output,session) {
   # List the first level callModules here
 }
--- inst/shinyexample/R/app_ui.R
+++ inst/shinyexample/R/app_ui.R
@@ -1,3 +1,5 @@
+#' @import shiny
 app_ui <- function() {
+  h1("shinyexample")
   # List the first level UI elements here
 }
