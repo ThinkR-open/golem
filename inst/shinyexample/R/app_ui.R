9111d0d94caab5d4f41922d7ba406c369e77d7cf
Colin <colin@thinkr.fr>
2019-03-05 16:43:07 +0100

    wrapped UI elements in a tagList in app_ui.R

--- inst/shinyexample/R/app_ui.R
+++ inst/shinyexample/R/app_ui.R
@@ -1,14 +1,16 @@
 #' @import shiny
 app_ui <- function() {
-  fluidPage(
+  tagList(
     # Leave this function for adding external resources
     # from inst/app/www
     golem_add_external_resources(),
     golem::js(),
     golem::favicon(),
-    h1("shinyexample")
+    # List the first level UI elements here 
+    fluidPage(
+      h1("shinyexample")
+    )
   )
-  # List the first level UI elements here
 }
 
 golem_add_external_resources <- function(){
