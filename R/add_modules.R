104cc39e3fefddf54745a5facecbf0a8245beeea
Colin <colin@thinkr.fr>
2019-03-09 10:07:00 +0100

    add_dockerfile function + reorganized files

---
+++ R/add_modules.R
@@ -0,0 +1,79 @@
+#' Create a module
+#' 
+#' This function creates a module inside the `R/` folder, based 
+#' on a specific module structure. 
+#' 
+#' @param name The name of the module
+#' @param pkg Path to the root of the package. Default is `"."`.
+#' @note This function will prefix the `name` argument with `mod_`.
+#' @export
+#' @importFrom glue glue
+#' @importFrom cli cat_bullet
+#' @importFrom utils file.edit
+
+add_module <- function(name, pkg = "."){
+  old <- setwd(normalizePath(pkg))
+  on.exit(setwd(old))
+  where <- file.path(
+    "R", paste0("mod_", name, ".R")
+  )
+  if (file.exists(where)){
+    if (!yesno("File already exists, override?")){
+      return(invisible(NULL))
+    }
+  }
+  file.create(where)
+  write_there <- function(...){
+    write(..., file = where, append = TRUE)
+  }
+  glue <- function(...){
+    glue::glue(..., .open = "%", .close = "%")
+  }
+  write_there("# Module UI")
+  
+  write_there(glue("#' @title   mod_%name%_ui and mod_%name%"))
+  write_there("#' @description  A shiny Module.")
+  write_there("#'")
+  write_there("#' @param id shiny id")
+  write_there("#' @param input internal")
+  write_there("#' @param output internal")
+  write_there("#' @param session internal")
+  write_there("#'")
+  write_there("#' @keywords internal")
+  write_there("#' @export ") 
+  write_there("#' @importFrom shiny NS tagList ") 
+  
+  
+  write_there(glue("mod_%name%°ui <- function(id){"))
+  write_there("  ns <- NS(id)")
+  write_there("  tagList(")
+  write_there("  ")
+  write_there("  )")
+  write_there("}")
+  write_there("    ")
+  write_there(glue("#' @rdname mod_%name%"))
+  write_there("#' @export")
+  write_there("#' @keywords internal")
+  write_there("    ")
+  write_there(glue("mod_%name% <- function(input, output, session){"))
+  write_there("  ns <- session$ns")
+  write_there("}")
+  write_there("    ")
+  
+  write_there("## To be copied in the UI")
+  write_there(glue('# mod_%name%_ui("%name%_ui_1")'))
+  write_there("    ")
+  write_there("## To be copied in the server")
+  write_there(glue('# callModule(mod_%name%_server, "%name%_ui_1")'))
+  write_there(" ")
+  cat_bullet(glue("File created at %where%"), bullet = "tick", bullet_col = "green")
+  if (rstudioapi::isAvailable()){
+    rstudioapi::navigateToFile(where)
+  } else {
+    cat_bullet(
+      glue("Go to %where%"), 
+      bullet = "square_small_filled", 
+      bullet_col = "red"
+    )
+  }
+}
---
+++ R/deploy_helpers.R
@@ -0,0 +1,67 @@
+#' Add an app.R at the root of your package to deploy on RStudio Connect
+#'
+#' @inheritParams add_module
+#' @importFrom cli cat_bullet
+#' @export
+add_rconnect_file <- function(pkg = "."){
+  where <- file.path(pkg, "app.R")
+  write_there <- function(..., here = where){
+    write(..., here, append = TRUE)
+  }
+  file.create( where )
+  usethis::use_build_ignore( where )
+  write_there("# To deploy, run: rsconnect::deployApp()")
+  write_there("")
+  write_there("pkgload::load_all()")
+  write_there("options( \"golem.app.prod\" = TRUE)")
+  write_there("shiny::shinyApp(ui = app_ui(), server = app_server)")
+  usethis::use_build_ignore(where)
+  usethis::use_package("pkgload")
+  cat_bullet(glue("File created at {where}"), bullet = "tick", bullet_col = "green")
+  cat_bullet("To deploy, run:")
+  cat(darkgrey("rsconnect::deployApp()\n"))
+  
+  
+  if (rstudioapi::isAvailable()){
+    rstudioapi::navigateToFile(where)
+  } else {
+    cat_bullet(
+      glue::glue("Go to {where}"), 
+      bullet = "square_small_filled", 
+      bullet_col = "red"
+    )
+  }
+  
+}
+
+#' Create a proper Dockerfile 
+#' 
+#' build a container containing your shiny app
+#'
+#' @param from path to the DESCRIPTION file.
+#' @param filename path to Dockerfile
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' add_dockerfile()
+#'}
+add_dockerfile <- function( path = "DESCRIPTION", output = "Dockerfile" ){
+  docker <- c(
+    glue::glue("FROM rocker/tidyverse:{major}.{minor}",major= R.Version()$major,minor= R.Version()$minor),
+    glue::glue('RUN R -e "install.packages(\'remotes\')"'),
+    glue::glue('RUN R -e "remotes::install_cran(\'{hop}\')"',
+               hop =att_from_description(path = path)),
+    glue::glue("COPY {read.dcf(path)[1]}_*.tar.gz  /app.tar.gz"),
+    "RUN R -e \"install.packages('/app.tar.gz', repos = NULL, type = 'source')\"",
+    
+    "EXPOSE 3838",
+    glue::glue("CMD [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0');{read.dcf(path)[1]}::run_app()\"]"))
+  docker <- paste(docker,collapse = " \n")
+  cat(docker,file=output)
+  cat_bullet(glue::glue("Be sure to put your {read.dcf(path)[1]}_{read.dcf('DESCRIPTION')[1,][['Version']]}.tar.gz file (generated using devtools::build() ) in the same folder as the {basename(output)} file generated"))
+  usethis::use_build_ignore(files = output)
+  
+  invisible(output)
+}
+
