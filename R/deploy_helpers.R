241fe15367b7e54d767dc10e8feb6bff350a8851
Colin <colin@thinkr.fr>
2019-03-19 13:15:04 +0100

    Bug correction from add_dockerfile

--- R/deploy_helpers.R
+++ R/deploy_helpers.R
@@ -1,5 +1,6 @@
 #' Add an app.R at the root of your package to deploy on RStudio Connect
 #'
+#' @param pkg Where to put the app.R.
 #' @inheritParams add_module
 #' @importFrom cli cat_bullet
 #' @export
@@ -60,15 +61,28 @@ add_dockerfile <- function( input = "DESCRIPTION", output = "Dockerfile" ){
   } 
   
   docker <- c(
-    glue::glue("FROM rocker/tidyverse:{major}.{minor}",major= R.Version()$major,minor= R.Version()$minor),
-    glue::glue('RUN R -e "install.packages(\'remotes\')"'),
-    glue::glue('RUN R -e "remotes::install_cran(\'{hop}\')"',
-               hop =att_from_description(path = path)),
-    glue::glue("COPY {read.dcf(path)[1]}_*.tar.gz  /app.tar.gz"),
+    glue::glue(
+      "FROM rocker/tidyverse:{major}.{minor}",
+      major= R.Version()$major,
+      minor= R.Version()$minor
+    ),
+    glue::glue(
+      'RUN R -e "install.packages(\'remotes\')"'
+    ),
+    glue::glue(
+      'RUN R -e "remotes::install_cran(\'{hop}\')"',
+      hop = att_from_description( path = input )
+    ),
+    glue::glue(
+      "COPY {read.dcf(input)[1]}_*.tar.gz  /app.tar.gz"
+    ),
     "RUN R -e \"install.packages('/app.tar.gz', repos = NULL, type = 'source')\"",
-    
     "EXPOSE 3838",
-    glue::glue("CMD [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0');{read.dcf(path)[1]}::run_app()\"]"))
+    glue::glue(
+      "CMD [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0');
+      {read.dcf(path)[1]}::run_app()\"]"
+    )
+  )
   docker <- paste(docker,collapse = " \n")
   cat(docker,file=output)
   cat_bullet(glue::glue("Be sure to put your {read.dcf(path)[1]}_{read.dcf('DESCRIPTION')[1,][['Version']]}.tar.gz file (generated using devtools::build() ) in the same folder as the {basename(output)} file generated"))
