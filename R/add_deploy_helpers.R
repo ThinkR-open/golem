84fb92295c0bd25a72f58f4563ce485c0216a2e2
Colin <colin@thinkr.fr>
2019-03-19 22:05:36 +0100

    refactor the deployers

--- R/add_deploy_helpers.R
+++ R/add_deploy_helpers.R
@@ -4,6 +4,7 @@
 #' @inheritParams add_module
 #' @importFrom cli cat_bullet
 #' @export
+#' 
 add_rconnect_file <- function(
   pkg = "."
 ){
@@ -51,7 +52,9 @@ add_rconnect_file <- function(
 #' @param from The FROM of the Dockerfile. Default is FROM rocker/tidyverse:
 #'     with `R.Version()$major` and `R.Version()$minor`.
 #'
+#' @importFrom dockerfiler dock_from_desc
 #' @export
+#' @rdname dockerfiles
 #' @examples
 #' \dontrun{
 #' add_shinyproxy_dockerfile()
@@ -59,7 +62,12 @@ add_rconnect_file <- function(
 add_shinyproxy_dockerfile <- function( 
   input = "DESCRIPTION", 
   output = "Dockerfile", 
-  from = paste0("FROM rocker/tidyverse:", R.Version()$major,".", R.Version()$minor)
+  from = paste0(
+    "rocker/tidyverse:", 
+    R.Version()$major,".", 
+    R.Version()$minor
+  ), 
+  as = NULL
 ){
   where <- file.path(output)
   
@@ -67,34 +75,87 @@ add_shinyproxy_dockerfile <- function(
     return(invisible(FALSE))
   } 
   
-  docker <- c(
-    from,
-    glue::glue(
-      'RUN R -e "install.packages(\'remotes\')"'
-    ),
-    glue::glue(
-      'RUN R -e "remotes::install_cran(\'{hop}\')"',
-      hop = att_from_description( path = input )
-    ),
-    glue::glue(
-      'RUN R -e "remotes::install_github(\'{hop}\')"',
-      hop = att_from_description( path = input )
-    ),
-    glue::glue(
-      "COPY {read.dcf(input)[1]}_*.tar.gz  /app.tar.gz"
-    ),
-    
-    "RUN R -e \"install.packages('/app.tar.gz', repos = NULL, type = 'source')\"",
-    "EXPOSE 3838",
-    glue::glue(
-      "CMD [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0'); {read.dcf(input)[1]}::run_app()\"]"
-    )
+  dock <- dock_from_desc(input, FROM = from, AS = as)
+  dock$EXPOSE(3838)
+  dock$CMD(glue::glue(
+    " [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0'); {read.dcf(input)[1]}::run_app()\"]"
+  ))
+  dock$write(output)
+  
+  cat_green_tick(
+    glue("Dockerfile created at {output}")
   )
-  docker <- paste(docker,collapse = " \n")
-  cat(docker,file=output)
-  cat_bullet(glue::glue("Be sure to put your {read.dcf(input)[1]}_{read.dcf('DESCRIPTION')[1,][['Version']]}.tar.gz file (generated using devtools::build() ) in the same folder as the {basename(output)} file generated"))
+  
+  alert_build(input, output)
+  
   usethis::use_build_ignore(files = output)
   
   invisible(output)
+  
 }
 
+#' @export
+#' @rdname dockerfiles
+add_heroku_dockerfile <- function( 
+  input = "DESCRIPTION", 
+  output = "Dockerfile", 
+  from = paste0(
+    "rocker/tidyverse:", 
+    R.Version()$major,".", 
+    R.Version()$minor
+  ), 
+  as = NULL
+){
+  where <- file.path(output)
+  
+  if ( !check_file_exist(where) ) {
+    return(invisible(FALSE))
+  } 
+  
+  dock <- dock_from_desc(input, FROM = from, AS = as)
+  
+  dock$CMD(glue::glue(
+    " [\"R\", \"-e options('shiny.port'=$PORT,shiny.host='0.0.0.0'); {read.dcf(input)[1]}::run_app()\"]"
+  ))
+  dock$write(output)
+  
+  cat_green_tick(
+    glue("Dockerfile created at {output}")
+  )
+  
+  apps_h <- gsub("\\.", "-", glue("{read.dcf(input)[1]}-{read.dcf('DESCRIPTION')[1,][['Version']]}"))
+  
+  alert_build(input, output)
+  
+  cat_rule( "From your command line, run:" )
+  cat_line("heroku container:login")
+  cat_line(
+    glue("heroku create {apps_h}")
+  ) 
+  cat_line(
+    glue("heroku container:push web --app {apps_h}")
+  )
+  cat_line(
+    glue("heroku container:release web --app {apps_h}")
+  )
+  cat_line(
+    glue("heroku open {apps_h}")
+  )
+  cat_red_bullet("Be sure to have the heroku CLI installed.")
+  cat_red_bullet(
+    glue("You can replace {apps_h} with another app name.")
+  )
+  
+  usethis::use_build_ignore(files = output)
+  
+  invisible(output)
+  
+}
+
+alert_build <- function(input, output){
+  cat_red_bullet(
+    glue::glue(
+      "Be sure to put your {read.dcf(input)[1]}_{read.dcf(input)[1,][['Version']]}.tar.gz file (generated using `devtools::build()` ) in the same folder as the {basename(output)} file generated"
+    )
+  )
+}
