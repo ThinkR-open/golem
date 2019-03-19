f69945fa2ed0c2b8caf8a7c883b56bf1990b344c
Colin <colin@thinkr.fr>
2019-03-19 22:16:13 +0100

    using dockerfiler function directly (copied and pasted) till it's fully deployed on CRAN

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,50 @@ replace_word <- function(file,pattern, replace){
   tx2  <- gsub(pattern = pattern, replacement = replace, x = tx)
   writeLines(tx2, con=file)
 }
+
+# From {dockerfiler}, in wait for the version to be on CRAN
+# 
+dock_from_desc <- function(
+  path = "DESCRIPTION",
+  FROM = "rocker/r-base",
+  AS = NULL
+){
+  
+  x <- dockerfiler::Dockerfile$new(FROM, AS)
+  x$RUN("R -e 'install.packages(\"remotes\")'")
+  
+  # We need to be sure install_cran is there
+  x$RUN("R -e 'remotes::install_github(\"r-lib/remotes\", ref = \"6c8fdaa\")'")
+  
+  desc <- read.dcf(path)
+  
+  # Handle cases where there is no deps
+  imp <- attempt::attempt({
+    desc[, "Imports"]
+  }, silent = TRUE)
+  
+  if (class(imp)[1] != "try-error"){
+    # Remove base packages which are not on CRAN
+    # And shouldn't be installed
+    reco <- rownames(installed.packages(priority="base"))
+    
+    if (length(imp) > 0) {
+      
+      imp <- gsub(",", "", imp)
+      imp <- strsplit(imp, "\n")[[1]]
+      for (i in seq_along(imp)){
+        if (!(imp[i] %in% reco)){
+          x$RUN(paste0("R -e 'remotes::install_cran(\"", imp[i], "\")'"))
+        }
+      }
+    }
+  }
+  
+  x$COPY(
+    from = paste0(desc[1], "_*.tar.gz"),
+    to = "/app.tar.gz"
+  )
+  x$RUN("R -e 'remotes::install_local(\"/app.tar.gz\")'")
+  
+  x
+}
