f9674e710283a3a28b5faf2ebd442593ccb01bc0
Colin <colin@thinkr.fr>
2019-03-20 16:46:15 +0100

    split into three dockerfile generation functions

--- R/add_deploy_helpers.R
+++ R/add_deploy_helpers.R
@@ -4,15 +4,12 @@
 #' @inheritParams add_module
 #' @importFrom cli cat_bullet
 #' @export
-#' 
 add_rconnect_file <- function(
   pkg = "."
 ){
   where <- file.path(pkg, "app.R")
   
-  if ( !check_file_exist(where) ) {
-    return(invisible(FALSE))
-  } 
+  if ( !check_file_exist(where) ) return(invisible(FALSE))
   
   write_there <- function(..., here = where){
     write(..., here, append = TRUE)
@@ -43,21 +40,60 @@ add_rconnect_file <- function(
 
 #' Create a Dockerfile for  Shiny App 
 #' 
-#' Build a container containing your Shiny App.
+#' Build a container containing your Shiny App. `add_dockerfile()` creates 
+#' a "generalistic" Dockerfile, while `add_shinyproxy_dockerfile()` and 
+#' `add_heroku_dockerfile()` creates plateform specific Dockerfile.
 #'
 #' @param input path to the DESCRIPTION file to use as an input.
 #' @param output name of the Dockerfile output.
 #' @param from The FROM of the Dockerfile. Default is FROM rocker/tidyverse:
 #'     with `R.Version()$major` and `R.Version()$minor`.
-#'
+#' @param as The AS of the Dockerfile. Default it NULL. 
+#' @param port The `options('shiny.port')` on which to run the Shiny App.
+#'     Default is 80.  
+#' @param host The `options('shiny.host')` on which to run the Shiny App.
+#'    Default is 0.0.0.0.  
 #' @importFrom dockerfiler dock_from_desc
 #' @export
 #' @rdname dockerfiles
 #' @examples
 #' \dontrun{
+#' add_dockerfile()
 #' add_shinyproxy_dockerfile()
+#' add_heroku_dockerfile()
 #'}
-add_shinyproxy_dockerfile <- function( 
+
+add_dockerfile <- function(
+  input = "DESCRIPTION", 
+  output = "Dockerfile", 
+  from = paste0(
+    "rocker/tidyverse:", 
+    R.Version()$major,".", 
+    R.Version()$minor
+  ), 
+  as = NULL, 
+  port = 80, 
+  host = "0.0.0.0"
+) {
+  
+  where <- file.path(output)
+  if ( !check_file_exist(where) ) return(invisible(FALSE))
+  
+  dock <- dock_from_desc(input, FROM = from, AS = as)
+  dock$EXPOSE(port)
+  dock$CMD(
+    glue::glue(
+      "R -e \"options('shiny.port'={port},shiny.host={host});{read.dcf(input)[1]}::run_app()\""
+    )
+  )
+  dock$write(output)
+  alert_build(input, output)
+  
+}
+
+#' @export
+#' @rdname dockerfiles
+add_dockerfile_shinyproxy <- function( 
   input = "DESCRIPTION", 
   output = "Dockerfile", 
   from = paste0(
@@ -67,22 +103,18 @@ add_shinyproxy_dockerfile <- function(
   ), 
   as = NULL
 ){
+  
   where <- file.path(output)
   
-  if ( !check_file_exist(where) ) {
-    return(invisible(FALSE))
-  } 
+  if ( !check_file_exist(where) ) return(invisible(FALSE))
   
   dock <- dock_from_desc(input, FROM = from, AS = as)
+  
   dock$EXPOSE(3838)
   dock$CMD(glue::glue(
     " [\"R\", \"-e options('shiny.port'=3838,shiny.host='0.0.0.0'); {read.dcf(input)[1]}::run_app()\"]"
   ))
   dock$write(output)
-  
-  cat_green_tick(
-    glue("Dockerfile created at {output}")
-  )
   
   alert_build(input, output)
   
@@ -94,7 +126,7 @@ add_shinyproxy_dockerfile <- function(
 
 #' @export
 #' @rdname dockerfiles
-add_heroku_dockerfile <- function( 
+add_dockerfile_heroku <- function( 
   input = "DESCRIPTION", 
   output = "Dockerfile", 
   from = paste0(
@@ -112,21 +144,19 @@ add_heroku_dockerfile <- function(
   
   dock <- dock_from_desc(input, FROM = from, AS = as)
   
-  dock$CMD(glue::glue(
-    "R -e \"options('shiny.port'=$PORT,shiny.host='0.0.0.0');{read.dcf(input)[1]}::run_app()\""
-  ))
+  dock$CMD(
+    glue::glue(
+      "R -e \"options('shiny.port'=$PORT,shiny.host='0.0.0.0');{read.dcf(input)[1]}::run_app()\""
+    )
+  )
   dock$write(output)
   
-  cat_green_tick(
-    glue("Dockerfile created at {output}")
-  )
+  alert_build(input, output)
   
   apps_h <- gsub(
     "\\.", "-", 
     glue("{read.dcf(input)[1]}-{read.dcf('DESCRIPTION')[1,][['Version']]}")
   )
-  
-  alert_build(input, output)
   
   cat_rule( "From your command line, run:" )
   cat_line("heroku container:login")
@@ -148,12 +178,14 @@ add_heroku_dockerfile <- function(
   )
   
   usethis::use_build_ignore(files = output)
-  
   invisible(output)
   
 }
 
 alert_build <- function(input, output){
+  cat_green_tick(
+    glue("Dockerfile created at {output}")
+  )
   cat_red_bullet(
     glue::glue(
       "Be sure to put your {read.dcf(input)[1]}_{read.dcf(input)[1,][['Version']]}.tar.gz file (generated using `devtools::build()` ) in the same folder as the {basename(output)} file generated"
