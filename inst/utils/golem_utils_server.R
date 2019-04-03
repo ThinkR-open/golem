97b24f3fcd52f1ea8cb4016aecb8f8abf686047b
Colin <colin@thinkr.fr>
2019-04-03 12:08:25 +0200

    added some info on the server functions

--- inst/utils/golem_utils_server.R
+++ inst/utils/golem_utils_server.R
@@ -1,13 +1,16 @@
+# Inverted versions of in, is.null and is.na
 `%not_in%` <- Negate(`%in%`)
 
 not_null <- Negate(is.null)
 
 not_na <- Negate(is.na)
 
+# Removes the null from a vector
 drop_nulls <- function(x){
   x[!sapply(x, is.null)]
 }
 
+# If x is null, return y, otherwise return x
 "%||%" <- function(x, y){
   if (is.null(x)) {
     y
@@ -15,7 +18,7 @@ drop_nulls <- function(x){
     x
   }
 }
-
+# If x is NA, return y, otherwise return x
 "%|NA|%" <- function(x, y){
   if (is.na(x)) {
     y
