f4ef755d42badfe713455bb5731c67709aa3e716
Colin <colin@thinkr.fr>
2019-04-04 10:16:16 +0200

    deploy

--- R/add_deploy_helpers.R
+++ R/add_deploy_helpers.R
@@ -192,3 +192,63 @@ alert_build <- function(input, output){
     )
   )
 }
+
+# From {dockerfiler}, in wait for the version to be on CRAN
+#' @importFrom utils installed.packages
+dock_from_desc <- function(
+  path = "DESCRIPTION",
+  FROM = "rocker/r-base",
+  AS = NULL
+){
+  
+  x <- dockerfiler::Dockerfile$new(FROM, AS)
+  x$RUN("R -e 'install.packages(\"remotes\")'")
+  
+  # We need to be sure install_cran is there
+  x$RUN("R -e 'remotes::install_github(\"r-lib/remotes\", ref = \"97bbf81\")'")
+  
+  desc <- read.dcf(path)
+  
+  # Handle cases where there is no deps
+  imp <- attempt::attempt({
+    desc[, "Imports"]
+  }, silent = TRUE)
+  
+  if (class(imp)[1] != "try-error"){
+    # Remove base packages which are not on CRAN
+    # And shouldn't be installed
+    reco <- rownames(installed.packages(priority="base"))
+    
+    # And Remotes package, which will be handled 
+    # by install_local
+    rem <- attempt::attempt({
+      desc[, "Remotes"]
+    }, silent = TRUE)
+    
+    if (class(rem)[1] != "try-error"){
+      rem <- strsplit(rem, "\n")[[1]]
+      rem <- vapply(rem, function(x){
+        strsplit(x, "/")[[1]][2]
+      }, character(1))
+      reco <- c(reco, unname(rem))
+    }
+    
+    if (length(imp) > 0) {
+      imp <- gsub(",", "", imp)
+      imp <- strsplit(imp, "\n")[[1]]
+      for (i in seq_along(imp)){
+        if (!(imp[i] %in% reco)){
+          x$RUN(paste0("R -e 'remotes::install_cran(\"", imp[i], "\")'"))
+        }
+      }
+    }
+  }
+  
+  x$COPY(
+    from = paste0(desc[1], "_*.tar.gz"),
+    to = "/app.tar.gz"
+  )
+  x$RUN("R -e 'remotes::install_local(\"/app.tar.gz\")'")
+  
+  x
+}
