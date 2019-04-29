cd5e5c4f2dd43c1c3908476a9336260286cb4b91
Colin <colin@thinkr.fr>
2019-04-29 23:53:40 +0200

    added tests helpers

---
+++ R/test_helpers.R
@@ -0,0 +1,34 @@
+#' Test helpers
+#' 
+#' These functions are designed to be used inside the tests 
+#' in your Shiny app package.
+#'
+#' @param object the object to test
+#'
+#' @return A testthat result
+#' @export
+#' @rdname testhelpers
+#'
+#' @examples
+#' expect_shinytag(1)
+expect_shinytag <- function(object) {
+  act <- testthat::quasi_label(rlang::enquo(object), arg = "object")
+  act$class <- class(object)
+  expect(
+    act$class == "shiny.tag",
+    sprintf("%s has class %s, not class 'shiny.tag'.", act$lab, act$class)
+  )
+  invisible(act$val)
+}
+
+#' @export
+#' @rdname testhelpers
+expect_shinytaglist <- function(object) {
+  act <- testthat::quasi_label(rlang::enquo(object), arg = "object")
+  act$class <- class(object)
+  expect(
+    act$class == "shiny.tag.list",
+    sprintf("%s has class %s, not class 'shiny.tag.list'.", act$lab, act$class)
+  )
+  invisible(act$val)
+}
--- inst/utils/test-golem-recommended.R
+++ inst/utils/test-golem-recommended.R
@@ -1,11 +1,14 @@
 context("golem tests")
 
+library(golem)
+
 test_that("app ui", {
   ui <- app_ui()
-  expect_is(ui, "shiny.tag.list")
+  expect_shinytaglist(ui, "shiny.tag.list")
 })
 
 test_that("app server", {
   server <- app_server
   expect_is(server, "function")
 })
+
