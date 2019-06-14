6a6441fd74ed768f1f866b1011a2feaefa80a98a
colin <colin@thinkr.fr>
2019-06-14 13:38:39 +0200

    reorder 01_start.R. Close #72, close #70, close #69.

--- inst/shinyexample/dev/01_start.R
+++ inst/shinyexample/dev/01_start.R
@@ -26,17 +26,18 @@ golem::set_golem_options()
 ## 
 ## If you want to use the MIT licence, README, code of conduct, lifecycle badge, and news
 
-usethis::use_git()
-usethis::use_mit_license(name = "Your Name")  # You can set another licence here
-usethis::use_readme_rmd()
+usethis::use_mit_license( name = "Your Name" )  # You can set another licence here
+usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
-usethis::use_lifecycle_badge("Experimental")
-usethis::use_news_md()
+usethis::use_lifecycle_badge( "Experimental" )
+
+usethis::use_news_md( open = FALSE )
+usethis::use_git()
 
 ## 1.3 - Add a data-raw folder
 ## 
 ## If you have data in your package
-usethis::use_data_raw()
+usethis::use_data_raw( open = FALSE )
 
 ## 1.4 - Init Tests
 ## 
@@ -54,9 +55,9 @@ golem::use_utils_ui()
 golem::use_utils_server()
 
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon( path = "inst/app/www/favicon.ico")
+golem::use_favicon( path = "inst/app/www/favicon.ico" )
 
 # You're now set! 
 # go to dev/02_dev.R
-rstudioapi::navigateToFile("dev/02_dev.R")
+rstudioapi::navigateToFile( "dev/02_dev.R" )
 
