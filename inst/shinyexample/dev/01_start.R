1c919af1a4710fbf8f22a8fcb7b16c636e1e13ea
colin <colin@thinkr.fr>
2019-06-14 12:18:44 +0200

    desc and mail to fit the format

--- inst/shinyexample/dev/01_start.R
+++ inst/shinyexample/dev/01_start.R
@@ -11,10 +11,10 @@
 golem::fill_desc(
   pkg_name = "shinyexample", # The Name of the package containing the App 
   pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC", # The Description of the package containing the App 
+  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
   author_first_name = "AUTHOR_FIRST", # Your First Name
   author_last_name = "AUTHOR_LAST",  # Your Last Name
-  author_email = "AUTHOR_MAIL",      # Your Email
+  author_email = "AUTHOR@MAIL.COM",      # Your Email
   repo_url = NULL # The (optional) URL of the GitHub Repo
 )     
 
