52175d0fe68e3b2b497cee6ca2c3fe91fd61cb87
colin <colin@thinkr.fr>
2019-06-26 21:32:34 +0200

    changed run_app for with_golem_options

--- inst/shinyexample/R/run_app.R
+++ inst/shinyexample/R/run_app.R
@@ -1,7 +1,11 @@
 #' Run the Shiny Application
 #'
 #' @export
-#' @importFrom shiny runApp
-run_app <- function() {
-  shiny::runApp(system.file("app", package = "shinyexample"))
+#' @importFrom shiny shinyApp
+#' @importFrom golem with_golem_options
+run_app <- function(...) {
+  with_golem_options(
+    app = shinyApp(ui = app_ui(), server = app_server), 
+    golem_opts = list(...)
+  )
 }
