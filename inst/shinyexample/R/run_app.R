8598d26bead624b38fe26340934cfca9e0e81eb4
colin <colin@thinkr.fr>
2019-07-01 15:27:07 +0200

    golemoptions now works in UI

--- inst/shinyexample/R/run_app.R
+++ inst/shinyexample/R/run_app.R
@@ -5,7 +5,7 @@
 #' @importFrom golem with_golem_options
 run_app <- function(...) {
   with_golem_options(
-    app = shinyApp(ui = app_ui(), server = app_server), 
+    app = shinyApp(ui = app_ui, server = app_server), 
     golem_opts = list(...)
   )
 }
