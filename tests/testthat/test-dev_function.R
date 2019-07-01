201554a69e751b4d5a78a08269ca119bb38a36fd
Vincent Guyader <vincent@thinkr.fr>
2019-07-01 17:11:29 +0200

    revert to expect_as_ref for browser_button

--- tests/testthat/test-dev_function.R
+++ tests/testthat/test-dev_function.R
@@ -43,16 +43,16 @@ test_that("test browser_button",{
  
     output <- capture_output_lines(browser_button())
     # normal_text <- readLines("browser_button.txt")
-    normal_text <- c("-- To be copied in your UI -----------------------------------------------------", 
-                     "actionButton(\"browser\", \"browser\"),", "tags$script(\"$('#browser').hide();\")", 
-                     "", "-- To be copied in your server -------------------------------------------------", 
-                     "observeEvent(input$browser,{", "  browser()", "})", "", "By default, this button will be hidden.", 
-                     "To show it, open your web browser JavaScript console", "And run $('#browser').show();"
-    )
+    # normal_text <- c("-- To be copied in your UI -----------------------------------------------------", 
+    #                  "actionButton(\"browser\", \"browser\"),", "tags$script(\"$('#browser').hide();\")", 
+    #                  "", "-- To be copied in your server -------------------------------------------------", 
+    #                  "observeEvent(input$browser,{", "  browser()", "})", "", "By default, this button will be hidden.", 
+    #                  "To show it, open your web browser JavaScript console", "And run $('#browser').show();"
+    # )
     # normal_text <- readRDS("browser_button.RDS")
-    expect_equal(output,normal_text)
+    # expect_equal(output,normal_text)
     
-    # expect_equal_to_reference(output,"browser_button.test")
+    expect_equal_to_reference(output,"browser_button.test")
 })
 
 test_that("test set_option",{
