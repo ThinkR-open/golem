fd914072dc77fc7cb768831870af71fdf2b2566b
statnmap <statnmap@users.noreply.github.com>
2019-07-19 10:36:49 +0200

    test-expect: revert

--- tests/testthat/test-expect_function.R
+++ tests/testthat/test-expect_function.R
@@ -1,29 +1,21 @@
 context("tests expect fonctions")
 
-## fake package
-fakename <- paste0(sample(letters, 10, TRUE), collapse = "")
-tpdir <- tempdir()
-if(!dir.exists(file.path(tpdir,fakename))){
-  create_golem(file.path(tpdir, fakename), open = FALSE)
-}
-pkg_expect <- file.path(tpdir, fakename)
-
 test_that("test expect_shinytag",{
-  with_dir(pkg_expect,{
+  with_dir(pkg,{
     expect_equal(capture_output(expect_shinytag(favicon("jean"))),"")
     expect_error(expect_shinytag("pierre"))
   })
 })
 
 test_that("test expect_shinytaglist",{
-  with_dir(pkg_expect,{
+  with_dir(pkg,{
     expect_equal(capture_output(expect_shinytaglist(shiny::tagList())),"")
     expect_error(expect_shinytaglist('test'))
   })
 })
 
 test_that("test expect_shinytaglist",{
-  with_dir(pkg_expect,{
+  with_dir(pkg,{
     
     ui <- shiny::h1("Jean")
     htmltools::save_html(ui,file = "jean.html")
