340a07290cbb7aee0faf5bca03b36d4175506002
statnmap <statnmap@users.noreply.github.com>
2019-07-19 10:23:13 +0200

    tests: reload independant

--- tests/testthat/test-zreload.R
+++ tests/testthat/test-zreload.R
@@ -1,9 +1,15 @@
 context("tests reload")
 
-
+## fake package
+fakename <- paste0(sample(letters, 10, TRUE), collapse = "")
+tpdir <- tempdir()
+if(!dir.exists(file.path(tpdir,fakename))){
+  create_golem(file.path(tpdir, fakename), open = FALSE)
+}
+pkg_reload <- file.path(tpdir, fakename)
 
 test_that("test document_and_reload",{
-  with_dir(pkg,{
+  with_dir(pkg_reload,{
     file.create("R/sum.R")
     cat(
     "#' @export
@@ -14,7 +20,7 @@ test_that("test document_and_reload",{
 })
 
 test_that("test detach_all_attached",{
-  with_dir(pkg,{
+  with_dir(pkg_reload,{
   test <- detach_all_attached()
   ok <- all(purrr::map_lgl(test,~ is.null(.x)))
   testthat::expect_true(ok)
