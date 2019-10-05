5bea0e69dffa0c4c10751cc75905e538fe8b58bb
Vincent Guyader <vincent@thinkr.fr>
2019-10-05 19:21:21 +0200

    remove miniCRAN depends

--- R/get_sysreqs.R
+++ R/get_sysreqs.R
@@ -3,11 +3,13 @@
 #'
 #' @param packages character vector of packages names
 #' @param quiet boolean if TRUE the function is quiet
-#' @importFrom miniCRAN pkgDep
 #' @importFrom utils download.file
 #' @importFrom jsonlite fromJSON
+#' @importFrom tools package_dependencies
 get_sysreqs <- function(packages, quiet = TRUE){
-all_deps <- paste(miniCRAN::pkgDep(packages,suggests = FALSE,quiet=quiet), collapse = ",")
+  
+  # all_deps <- paste(miniCRAN::pkgDep(packages,suggests = FALSE,quiet=quiet), collapse = ",")
+  all_deps <- paste(unique(unlist(tools::package_dependencies(packages))), collapse = ",")
 url <- sprintf("https://sysreqs.r-hub.io/pkg/%s/linux-x86_64-debian-gcc",all_deps)
 path <- tempfile()
 utils::download.file(url, path,mode = "wb",quiet = quiet)
