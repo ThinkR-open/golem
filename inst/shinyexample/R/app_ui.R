ca8ddb355e8c3a0bb659df43dbc647b9005ff5b1
novica <nnovica@gmail.com>
2019-10-06 09:20:22 +0200

    add a title tag to golem_add_external_resources

--- inst/shinyexample/R/app_ui.R
+++ inst/shinyexample/R/app_ui.R
@@ -19,10 +19,11 @@ golem_add_external_resources <- function(){
  
   tags$head(
     golem::activate_js(),
-    golem::favicon()
+    golem::favicon(),
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
     #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    tags$title("shinyexample")
   )
 }
