6d48dd5e605629ce501486752b62d29f1ee30975
Vincent Guyader <vincent@thinkr.fr>
2019-10-07 23:24:36 +0200

    use miniCRAN to get all pacakges_dependencies

--- R/get_sysreqs.R
+++ R/get_sysreqs.R
@@ -5,16 +5,15 @@
 #' @param quiet boolean if TRUE the function is quiet
 #' @importFrom utils download.file
 #' @importFrom jsonlite fromJSON
-#' @importFrom tools package_dependencies
+#' @importFrom miniCRAN pkgDep
 get_sysreqs <- function(packages, quiet = TRUE){
   
   # all_deps <- paste(miniCRAN::pkgDep(packages,suggests = FALSE,quiet=quiet), collapse = ",")
-  all_deps <- paste(unique(unlist(tools::package_dependencies(packages))), collapse = ",")
-all_deps <- c(packages,all_deps)
+  all_deps <- paste(unique(c(packages, unlist(miniCRAN::pkgDep(packages)))), collapse = ",")
   url <- sprintf("https://sysreqs.r-hub.io/pkg/%s/linux-x86_64-debian-gcc",all_deps)
-path <- tempfile()
-utils::download.file(url, path,mode = "wb",quiet = quiet)
-out <- jsonlite::fromJSON(path)
-unlink(path)
-sort(unique(out[!is.na(out)]))
+  path <- tempfile()
+  utils::download.file(url, path,mode = "wb",quiet = quiet)
+  out <- jsonlite::fromJSON(path)
+  unlink(path)
+  sort(unique(out[!is.na(out)]))
 }
