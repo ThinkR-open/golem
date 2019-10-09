b3d73d9d1b3cf4ed901edc3837060b71d8603f22
Ziwei Ye <zxy1677@rit.edu>
2019-10-09 19:36:12 +0900

    Revert "Added add_external_js_file and add_external_css_file function"
    This reverts commit a614c641b5c1b0276adc6c0213a48cf51679b278.

--- R/add_files.R
+++ R/add_files.R
@@ -59,65 +59,6 @@ add_js_file <- function(
 
 #' @export
 #' @rdname add_files
-add_external_js_file <- function(
-  url,
-  name,
-  pkg = get_golem_wd(), 
-  dir = "inst/app/www",
-  open = TRUE, 
-  dir_create = TRUE
-){
-  
-  old <- setwd(normalizePath(pkg))  
-  on.exit(setwd(old))
-  new_file <- glue::glue("{name}.js")
-  
-  dir_created <- create_dir_if_needed(
-    dir, 
-    dir_create
-  )
-  
-  if (!dir_created){
-    cat_red_bullet(
-      "File not added (needs a valid directory)"
-    )
-    return(invisible(FALSE))
-  }
-  
-  dir <- normalizePath(dir) 
-  
-  where <- file.path(
-    dir, new_file
-  )
-  if ( !check_file_exist(where) ) {
-    return(invisible(FALSE))
-  } 
-  
-  if ( tools::file_ext(url) != "js") {
-    cat_red_bullet(
-      "File not added (URL must end with .js extension)"
-    )
-    return(invisible(FALSE))
-  }
-  
-  utils::download.file(url, where)
-  
-  cat_green_tick(glue::glue("File created at {where}"))
-  cat_red_bullet(
-    glue::glue(
-      'To link to this file, go to the `golem_add_external_resources()` function in `app_ui.R` and add `tags$script(src="www/{name}.js")`'
-    )
-  )
-  
-  if (rstudioapi::isAvailable() & open){
-    rstudioapi::navigateToFile(where)
-  } else {
-    cat_red_bullet(glue::glue("Go to {where}"))
-  }
-}
-
-#' @export
-#' @rdname add_files
 add_js_handler <- function(
   name, 
   pkg = get_golem_wd(), 
@@ -227,65 +168,6 @@ add_css_file <- function(
   }
 }
 
-
-#' @export
-#' @rdname add_files
-add_external_css_file <- function(
-  url,
-  name,
-  pkg = get_golem_wd(), 
-  dir = "inst/app/www",
-  open = TRUE, 
-  dir_create = TRUE
-){
-  
-  old <- setwd(normalizePath(pkg))  
-  on.exit(setwd(old))
-  new_file <- glue::glue("{name}.css")
-  
-  dir_created <- create_dir_if_needed(
-    dir, 
-    dir_create
-  )
-  
-  if (!dir_created){
-    cat_red_bullet(
-      "File not added (needs a valid directory)"
-    )
-    return(invisible(FALSE))
-  }
-  
-  dir <- normalizePath(dir) 
-  
-  where <- file.path(
-    dir, new_file
-  )
-  if ( !check_file_exist(where) ) {
-    return(invisible(FALSE))
-  } 
-  
-  if ( tools::file_ext(url) != "css") {
-    cat_red_bullet(
-      "File not added (URL must end with .css extension)"
-    )
-    return(invisible(FALSE))
-  }
-  
-  utils::download.file(url, where)
-  
-  cat_green_tick(glue::glue("File created at {where}"))
-  cat_red_bullet(
-    glue::glue(
-      'To link to this file, go to the `golem_add_external_resources()` function in `app_ui.R` and add `tags$script(src="www/{name}.css")`'
-    )
-  )
-  
-  if (rstudioapi::isAvailable() & open){
-    rstudioapi::navigateToFile(where)
-  } else {
-    cat_red_bullet(glue::glue("Go to {where}"))
-  }
-}
 
 #' @export
 #' @rdname add_files
