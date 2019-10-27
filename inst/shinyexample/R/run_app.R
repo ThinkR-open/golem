f5fa1241fb145d65e45413b64778f1368e069068
chase <cclark42@uic.edu>
2019-10-27 13:24:42 -0500

    Allow passing arguments to shiny::shinyApp()
    https://github.com/ThinkR-open/golem/issues/128

--- inst/shinyexample/R/run_app.R
+++ inst/shinyexample/R/run_app.R
@@ -1,11 +1,23 @@
 #' Run the Shiny Application
 #'
+#' @param onStart optional, described in ?shiny::shinyApp
+#' @param options optional, described in ?shiny::shinyApp
+#' @param enableBookmarking optional, described in ?shiny::shinyApp
+#' @param ... arguments to pass to golem_opts
+#'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function(...) {
+run_app <- function(onStart = NULL,
+                    options = list(), 
+                    enableBookmarking = NULL,
+                    ...) {
   with_golem_options(
-    app = shinyApp(ui = app_ui, server = app_server), 
+    app = shinyApp(ui = app_ui,
+                   server = app_server,
+                   onStart = onStart,
+                   options = options, 
+                   enableBookmarking = enableBookmarking), 
     golem_opts = list(...)
   )
 }
