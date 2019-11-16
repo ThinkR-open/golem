aef0e133fe23c11bdc89f167ed417760c0807ecd
novica <nnovica@gmail.com>
2019-11-16 21:30:31 +0100

    move contents of test-add_rstudioconnect_file

--- tests/testthat/test-add_deploy_helpers.R
+++ tests/testthat/test-add_deploy_helpers.R
@@ -37,3 +37,35 @@ test_that("add_dockerfile_shinyproxy", {
     remove_files("Dockerfile")
   })
 })
+
+context("function add_rstudioconnect_file")
+
+test_that("add_rstudioconnect_file", {
+  with_dir(pkg, {
+    remove_file("app.R")
+    output <- testthat::capture_output(add_rstudioconnect_file(pkg = pkg, open = FALSE))
+    expect_true(file.exists("app.R"))
+    test <-
+      stringr::str_detect(output, "ile created at .*/app.R")
+    expect_true(test)
+    remove_file("app.R")
+  })
+  with_dir(pkg, {
+    remove_file("app.R")
+    output <- testthat::capture_output(add_shinyappsio_file(pkg = pkg,open = FALSE))
+    expect_true(file.exists("app.R"))
+    test <-
+      stringr::str_detect(output, "ile created at .*/app.R")
+    expect_true(test)
+    remove_file("app.R")
+  })
+  with_dir(pkg, {
+    remove_file("app.R")
+    output <- testthat::capture_output(add_shinyserver_file(pkg = pkg,open = FALSE))
+    expect_true(file.exists("app.R"))
+    test <-
+      stringr::str_detect(output, "ile created at .*/app.R")
+    expect_true(test)
+    remove_file("app.R")
+  })
+})
