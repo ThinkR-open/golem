0bc299c7f3a95ecf1ad7e70c7c7e9d559c7f335d
novica <nnovica@gmail.com>
2019-11-18 22:16:54 +0100

    move test for r files to new file

--- tests/testthat/test-add_files.R
+++ tests/testthat/test-add_files.R
@@ -91,29 +91,3 @@ test_that("add_ui_server_files", {
     remove_file("inst/app/server.R")
   })
 })
-
-test_that("add_fct and add_utils", {
-  with_dir(pkg, {
-    util_file <- "R/utils_ui.R"
-    fct_file <- "R/fct_ui.R"
-    mod <- "R/mod_plop.R"
-    remove_file(util_file)
-    remove_file(fct_file)
-    remove_file(mod)
-    add_fct("ui", pkg = pkg, open = FALSE)
-    add_utils("ui", pkg = pkg, open = FALSE)
-    
-    expect_true(file.exists(util_file))    
-    expect_true(file.exists(fct_file))    
-    rand <- rand_name()
-    add_module(rand, pkg = pkg, open = FALSE)
-    add_fct("ui", rand, pkg = pkg, open = FALSE)
-    add_utils("ui", rand, pkg = pkg, open = FALSE)
-    expect_true(file.exists(sprintf("R/mod_%s_fct_ui.R", rand)))    
-    expect_true(file.exists(sprintf("R/mod_%s_utils_ui.R", rand)))    
-    
-    remove_file(sprintf("R/mod_%s.R", rand))
-    remove_file(sprintf("R/mod_%s_fct_ui.R", rand))
-    remove_file(sprintf("R/mod_%s_utils_ui.R", rand))
-  })
-})
