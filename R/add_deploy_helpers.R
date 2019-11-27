e24cc1d709747d20fdf9b066ff1a4d40fe397a81
Vincent Guyader <vincent@thinkr.fr>
2019-11-27 19:26:38 +0100

    fix #306

--- R/add_deploy_helpers.R
+++ R/add_deploy_helpers.R
@@ -395,12 +395,19 @@ dock_from_desc <- function(
   packages_on_cran <- remotes_deps$package[remotes_deps$is_cran] %>% 
     intersect(packages)
   
+  
   packages_not_on_cran <- packages %>% 
     setdiff(packages_on_cran)
   
   
-  packages_on_cran <- setNames(lapply(packages_on_cran, packageVersion), packages_on_cran)
+ packages_with_version <-  data.frame(
+   package=remotes_deps$package,
+   installed=remotes_deps$installed,stringsAsFactors = FALSE
+  )%>%
+   filter(package %in% packages_on_cran)
+
   
+  packages_on_cran <- packages_with_version$installed %>% setNames(packages_with_version$package)
   
   dock <- dockerfiler::Dockerfile$new(FROM = FROM)
   
