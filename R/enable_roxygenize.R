b59f3fd81ace08b11026f8a88b920be428ab0226
Vincent Guyader <vincent@thinkr.fr>
2020-02-09 23:06:57 +0100

    allow multiple .Rproj editing at once

--- R/enable_roxygenize.R
+++ R/enable_roxygenize.R
@@ -5,20 +5,23 @@
 #'
 #' @export
 #'
-enable_roxygenize <- function(path = list.files(path = ".",pattern = "Rproj$",full.names = TRUE)[1]){
+enable_roxygenize <- function(path = list.files(path = ".",pattern = "Rproj$",full.names = TRUE)){
   cat_bullet(
     glue::glue("Read {basename(path)} content "), 
     bullet = "info",
     bullet_col = "green"
   )
-  source <-   yaml::read_yaml(file = path)
+  source <- lapply(path, yaml::read_yaml)
   cat_bullet(
     "Enable roxygen2", 
     bullet = "info",
     bullet_col = "green"
   )
-  source[["PackageRoxygenize"]]<- "rd,collate,namespace"
-  yaml::write_yaml(x = source,file=path)
-  
+  out <- lapply(source,function(x){
+    x[["PackageRoxygenize"]]<- "rd,collate,namespace"
+    x
+  })
+
+  mapply(yaml::write_yaml, out, path)
   cat_green_tick("Done") 
 }
