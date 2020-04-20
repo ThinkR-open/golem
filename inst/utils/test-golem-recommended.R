4eac63b287dc40e3c6a1ef95e3cf17d96b7ea8f8
novica <nnovica@gmail.com>
2020-04-20 13:47:27 +0200

    if (interactive()) {} in test_that( "app launches" ) #410

--- inst/utils/test-golem-recommended.R
+++ inst/utils/test-golem-recommended.R
@@ -12,29 +12,26 @@ test_that("app server", {
   expect_is(server, "function")
 })
 
+expect_running <- function(sleep){
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+  x <- processx::process$new(
+    "R", 
+    c(
+      "-e", 
+      "pkgload::load_all(here::here());run_app()"
+    )
+  )
+  Sys.sleep(sleep)
+  expect_true(x$is_alive())
+  x$kill()
+}
+
 # Configure this test to fit your need
 test_that(
   "app launches",{
-    skip_on_cran()
-    skip_on_travis()
-    skip_on_appveyor()
-    x <- processx::process$new(
-      "R", 
-      c(
-        "-e", 
-        "pkgload::load_all(here::here());run_app()"
-      )
-    )
-    Sys.sleep(5)
-    expect_true(x$is_alive())
-    x$kill()
+    skip_if_not(interactive())
+    expect_running(sleep = 5)
   }
 )
-
-
-
-
-
-
-
-
