afc8a15d588511baba6b93c66d1cefec641d8899
novica <nnovica@gmail.com>
2020-04-30 16:01:55 +0200

    move function to testhelpers

--- inst/utils/test-golem-recommended.R
+++ inst/utils/test-golem-recommended.R
@@ -12,22 +12,6 @@ test_that("app server", {
   expect_is(server, "function")
 })
 
-expect_running <- function(sleep){
-  skip_on_cran()
-  skip_on_travis()
-  skip_on_appveyor()
-  x <- processx::process$new(
-    "R", 
-    c(
-      "-e", 
-      "pkgload::load_all(here::here());run_app()"
-    )
-  )
-  Sys.sleep(sleep)
-  expect_true(x$is_alive())
-  x$kill()
-}
-
 # Configure this test to fit your need
 test_that(
   "app launches",{
