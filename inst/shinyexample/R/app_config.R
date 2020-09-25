09e26872175c36129b20926f4c2bd23f829110ca
colin <colin@thinkr.fr>
2020-09-25 11:23:01 +0200

    comment in app_config

--- inst/shinyexample/R/app_config.R
+++ inst/shinyexample/R/app_config.R
@@ -1,7 +1,11 @@
 #' Access files in the current app
 #' 
-#' @param ... Character vector specifying directory and or file to
-#'     point to inside the current package.
+#' NOTE: If you manually change your package name in the DESCRIPTION, 
+#' don't forget to change it here too, and in the config file. 
+#' For a safer name change mechanism, use the `golem::set_golem_name()` function.
+#' 
+#' @param ... character vectors, specifying subdirectory and file(s) 
+#' within your package. The default, none, returns the root of the app. 
 #' 
 #' @noRd
 app_sys <- function(...){
