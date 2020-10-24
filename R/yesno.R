a6e6f21025ca703cc5b78f6a429ce6c837a87c63
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-10-24 11:45:47 -0300

    Default value for .

--- R/yesno.R
+++ R/yesno.R
@@ -1,6 +1,12 @@
+# @param ... Arguments passed to the `cat` function.
+# @param default Boolean default value to return in case there is no user
+#      interaction.
+#
 #' @importFrom utils menu
-yesno <- function (...) 
-{
+yesno <- function(..., default = FALSE) {
+  if (!interactive()) {
+    return(default)
+  }
   cat(paste0(..., collapse = ""))
   menu(c("Yes", "No")) == 1
 }
