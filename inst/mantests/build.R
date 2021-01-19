a7d491e21ffcb2c24f7b5a168df7da8c526986ca
colin <colin@thinkr.fr>
2021-01-19 12:53:56 +0100

    devtools check pass

--- inst/mantests/build.R
+++ inst/mantests/build.R
@@ -36,7 +36,7 @@ library(desc, lib.loc = temp_lib)
 library(testthat, lib.loc = temp_lib)
 
 cli::cat_rule("Install crystalmountains")
-#remotes::install_github("thinkr-open/crystalmountains")
+remotes::install_github("thinkr-open/crystalmountains")
 
 cli::cat_rule("Copy golem to temp_golem and install it")
 dir.create(temp_golem, recursive = TRUE)
