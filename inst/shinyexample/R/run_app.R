67d2502ae395e2783a145d9821bd963750382663
colin <colin@thinkr.fr>
2021-02-19 09:21:51 +0100

    added a missing parameter to run_app

--- inst/shinyexample/R/run_app.R
+++ inst/shinyexample/R/run_app.R
@@ -1,15 +1,17 @@
 #' Run the Shiny Application
 #'
-#' @param ... arguments to pass to golem_opts
+#' @param ... arguments to pass to golem_opts. 
+#' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
 #'
 #' @export
 #' @importFrom shiny shinyApp
-#' @importFrom golem with_golem_options
+#' @importFrom golem with_golem_options 
 run_app <- function(
   onStart = NULL,
   options = list(), 
   enableBookmarking = NULL,
+  uiPattern = "/",
   ...
 ) {
   with_golem_options(
@@ -18,7 +20,8 @@ run_app <- function(
       server = app_server,
       onStart = onStart,
       options = options, 
-      enableBookmarking = enableBookmarking
+      enableBookmarking = enableBookmarking, 
+      uiPattern = uiPattern
     ), 
     golem_opts = list(...)
   )
