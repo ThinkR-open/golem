f0751439b53aabfdfcbdce60833e2d8bd576b102
colin <colin@thinkr.fr>
2021-03-05 09:02:21 +0100

    print(Sys.getenv())

--- inst/mantests/build.R
+++ inst/mantests/build.R
@@ -1,4 +1,4 @@
-Sys.getenv()
+print(Sys.getenv())
 
 # rstudioapi::jobRunScript(here::here("inst/mantests/build.R"), workingDir = here::here())
 cat_ok <- function() cli::cat_bullet("Passed", bullet = "tick", bullet_col = "green")
