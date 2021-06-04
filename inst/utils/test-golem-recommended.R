206d0e884aacc1c14d9b2268c7cc2d2a8e78e2bd
colin <colin@thinkr.fr>
2021-06-04 12:03:30 +0200

    moved to expect_type, close #671

--- inst/utils/test-golem-recommended.R
+++ inst/utils/test-golem-recommended.R
@@ -10,7 +10,7 @@ test_that("app ui", {
 
 test_that("app server", {
   server <- app_server
-  expect_is(server, "function")
+  expect_type(server, "closure")
   # Check that formals have not been removed
   fmls <- formals(app_server)
   for (i in c("input", "output", "session")){
