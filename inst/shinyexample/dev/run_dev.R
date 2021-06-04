9c094aa17efef3141d6f4fc661527cea5291f93e
Colin Fay <contact@colinfay.me>
2021-06-04 12:14:55 +0200

    Revert "close #675"
    This reverts commit 3f5634672883618e1ce6751092f3343fd297d696.

--- inst/shinyexample/dev/run_dev.R
+++ inst/shinyexample/dev/run_dev.R
@@ -1,9 +1,6 @@
 # Set options here
 options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
 
-# Comment this if you don't want the app to be served on a random port
-options(shiny.port = httpuv::randomPort()) 
-
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
 # rm(list=ls(all.names = TRUE))
