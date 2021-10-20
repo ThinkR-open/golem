1fc63ad51403dff749c738f8ed7442e0941ba374
ALanguillaume <antoinelanguillaume@gmail.com>
2021-10-20 15:31:45 +0200

    Replace calls to testhat::expect_is()
    By up-to-date alternatives.

--- tests/testthat/test-make_dev.R
+++ tests/testthat/test-make_dev.R
@@ -3,16 +3,16 @@ test_that("test app_dev",{
     c(
       golem.app.prod = TRUE
     ),{
-    expect_false(app_dev()) 
-    expect_true(app_prod()) 
-  })
+      expect_false(app_dev())
+      expect_true(app_prod())
+    })
   with_options(
     c(
       golem.app.prod = FALSE
     ),{
-    expect_true(app_dev()) 
-    expect_false(app_prod()) 
-  })
+      expect_true(app_dev())
+      expect_false(app_prod())
+    })
 })
 
 test_that("test print_dev",{
@@ -20,9 +20,9 @@ test_that("test print_dev",{
     c(
       golem.app.prod = FALSE
     ),{
-    expect_equal(print_dev("test"),"test")
-    expect_is(print_dev("test"), "character")
-  })
+      expect_equal(print_dev("test"),"test")
+      expect_type(print_dev("test"), "character")
+    })
 })
 
 test_that("test make_dev",{
@@ -30,12 +30,12 @@ test_that("test make_dev",{
     c(
       golem.app.prod = FALSE
     ),{
-    sum_dev <- make_dev(sum)
-    class(sum_dev)
-    expect_equal(sum_dev(1,2),3)
-    expect_is(sum_dev(1,2), "numeric")
-    expect_is(sum_dev, "function")
-  })
+      sum_dev <- make_dev(sum)
+      class(sum_dev)
+      expect_equal(sum_dev(1,2),3)
+      expect_type(sum_dev(1,2), "double")
+      expect_type(sum_dev, "closure")
+    })
 })
 
 test_that("test print_dev",{
@@ -43,30 +43,30 @@ test_that("test print_dev",{
     c(
       golem.app.prod = FALSE
     ),{
-    expect_equal(print_dev("test"),"test")
-    expect_is(print_dev("test"), "character")
-  })
+      expect_equal(print_dev("test"),"test")
+      expect_type(print_dev("test"), "character")
+    })
 })
 
 
 test_that("test browser_button",{
- 
-    output <- capture_output_lines(browser_button())
-    expect_true(
-      grepl('actionButton\\("browser", "browser"\\)', output[2])
-    )
-    expect_true(
-      grepl('tags\\$script\\(\"\\$\\(\'#browser\'\\).hide\\(\\);\"\\)', output[3])
-    )
-    expect_true(
-      grepl('observeEvent\\(input\\$browser', output[6])
-    )
-    expect_true(
-      grepl('  browser()', output[7])
-    )
-    expect_true(
-      grepl('run \\$\\(\'#browser\'\\)\\.show\\(\\);', output[12])
-    )
+  
+  output <- capture_output_lines(browser_button())
+  expect_true(
+    grepl('actionButton\\("browser", "browser"\\)', output[2])
+  )
+  expect_true(
+    grepl('tags\\$script\\(\"\\$\\(\'#browser\'\\).hide\\(\\);\"\\)', output[3])
+  )
+  expect_true(
+    grepl('observeEvent\\(input\\$browser', output[6])
+  )
+  expect_true(
+    grepl('  browser()', output[7])
+  )
+  expect_true(
+    grepl('run \\$\\(\'#browser\'\\)\\.show\\(\\);', output[12])
+  )
 })
 
 # test_that("test set_option",{
