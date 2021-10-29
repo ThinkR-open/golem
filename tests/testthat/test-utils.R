feae703bf6791436cfb0f1269358eea1fb6a8751
ALanguillaume <antoinelanguillaume@gmail.com>
2021-10-29 09:59:21 +0200

    Add tests for is_existing_modules()

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,14 @@
+
+dummy_golem <- tempfile(pattern = "test_is_existing_modules")
+dummy_golem_R <- file.path(dummy_golem, "R/")
+dir.create(dummy_golem_R, recursive = TRUE)
+dummy_module_files <- c("mod_main.R", "mod_left_pane.R")
+file.create(file.path(dummy_golem_R, dummy_module_files))
+
+withr::with_dir(dummy_golem, {
+  test_that("existing modules are properly detected", {
+    expect_false(is_existing_module("foo"))
+    expect_true(is_existing_module("left_pane"))
+    expect_true(is_existing_module("main"))
+  })
+})
