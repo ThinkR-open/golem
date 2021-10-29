ee044d23d9b3a229e3178b76d05be6b0ea2facfe
ALanguillaume <antoinelanguillaume@gmail.com>
2021-10-29 09:32:25 +0200

    Check module exists before adding module tests
    If not an error is thrown.

--- tests/testthat/test-modules_fn.R
+++ tests/testthat/test-modules_fn.R
@@ -1,3 +1,22 @@
+
+test_that("use_module_test", {
+  with_dir(pkg, {
+    remove_file("R/mod_mod1.R")
+    add_module("mod1", open = FALSE, pkg = pkg)
+    
+    use_module_test("mod1", pkg = pkg, open = FALSE)
+    expect_true(file.exists("tests/testthat/test-mod_mod1.R"))
+    
+    expect_error(
+      use_module_test("mod2", pkg = pkg, open = FALSE),
+      regex = "^The mentionned 'module' does not yet exist.$"
+    )
+    
+    remove_file("R/mod_mod1.R")
+    remove_file("tests/testthat/test-mod_mod1.R")
+  })
+})
+
 test_that("module_fn work", {
   expect_equal(
     mod_remove("mod_a"),
