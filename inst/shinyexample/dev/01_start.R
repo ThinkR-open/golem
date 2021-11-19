3cd1bef6e934275876804826427d0fafcc505776
colin <colin@thinkr.fr>
2021-11-19 13:48:34 +0100

    with_test = TRUE in dev scripts

--- inst/shinyexample/dev/01_start.R
+++ inst/shinyexample/dev/01_start.R
@@ -1,43 +1,43 @@
 # Building a Prod-Ready, Robust Shiny Application.
-# 
-# README: each step of the dev files is optional, and you don't have to 
-# fill every dev scripts before getting started. 
-# 01_start.R should be filled at start. 
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
 # 02_dev.R should be used to keep track of your development during the project.
 # 03_deploy.R should be used once you need to deploy your app.
-# 
-# 
+#
+#
 ########################################
 #### CURRENT FILE: ON START SCRIPT #####
 ########################################
 
 ## Fill the DESCRIPTION ----
 ## Add meta data about your application
-## 
-## /!\ Note: if you want to change the name of your app during development, 
+##
+## /!\ Note: if you want to change the name of your app during development,
 ## either re-run this function, call golem::set_golem_name(), or don't forget
 ## to change the name in the app_sys() function in app_config.R /!\
-## 
+##
 golem::fill_desc(
-  pkg_name = "shinyexample", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
+  pkg_name = "shinyexample", # The Name of the package containing the App
+  pkg_title = "PKG_TITLE", # The Title of the package containing the App
+  pkg_description = "PKG_DESC.", # The Description of the package containing the App
   author_first_name = "AUTHOR_FIRST", # Your First Name
   author_last_name = "AUTHOR_LAST", # Your Last Name
   author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
-)     
+  repo_url = NULL # The URL of the GitHub Repo (optional)
+)
 
 ## Set {golem} options ----
 golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( "Golem User" )  # You can set another license here
-usethis::use_readme_rmd( open = FALSE )
+usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_readme_rmd(open = FALSE)
 usethis::use_code_of_conduct()
-usethis::use_lifecycle_badge( "Experimental" )
-usethis::use_news_md( open = FALSE )
+usethis::use_lifecycle_badge("Experimental")
+usethis::use_news_md(open = FALSE)
 
 ## Use git ----
 usethis::use_git()
@@ -51,7 +51,7 @@ golem::use_recommended_deps()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+golem::use_favicon() # path = "path/to/ico". Can be an online file.
 golem::remove_favicon()
 
 ## Add helper functions ----
@@ -61,5 +61,4 @@ golem::use_utils_server()
 # You're now set! ----
 
 # go to dev/02_dev.R
-rstudioapi::navigateToFile( "dev/02_dev.R" )
-
+rstudioapi::navigateToFile("dev/02_dev.R")
