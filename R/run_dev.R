11a0fc91ffdc51c875c314d0531a8b7d861278b1
colin <colin@thinkr.fr>
2022-02-25 13:54:10 +0100

    run_dev() refactoring
    `golem::run_dev()` has been refactored to match
    the behavior of other functions, notably it
    now uses `golem::get_golem_wd()` to
    find the current working dir.

--- R/run_dev.R
+++ R/run_dev.R
@@ -1,38 +1,36 @@
 #' Run run_dev.R
 #'
 #' @param file File path to `run_dev.R`. Defaults to `R/run_dev.R`.
-#' @param pkg Package name to run the file. Defaults to current active package.
+#' @inheritParams add_module
 #'
 #' @export
 #'
 #' @return Used for side-effect
 run_dev <- function(
-  file,
-  pkg = pkgload::pkg_name()
+  file = "dev/run_dev.R",
+  pkg = get_golem_wd()
 ) {
-  if (missing(file)) {
-    file <- "dev/run_dev.R"
-  }
 
-  if (pkgload::is_dev_package(pkg)) {
+  # We'll look for the run_dev script in the current dir
+  try_dev <- file.path(
+    pkg,
+    file
+  )
+
+  # Stop if it doesn't exists
+  if (file.exists(try_dev)) {
     run_dev_lines <- readLines(
-      file.path(
-        pkgload::pkg_path(),
-        file
-      )
+      "dev/run_dev.R"
     )
   } else {
-    try_dev <- file.path(
-      pkgload::pkg_path(),
-      file
+    stop(
+      "Unable to locate dev file"
     )
-
-    if (file.exists(try_dev)) {
-      run_dev_lines <- readLines("dev/run_dev.R")
-    } else {
-      stop("Unable to locate dev file")
-    }
   }
 
-  eval(parse(text = run_dev_lines))
+  eval(
+    parse(
+      text = run_dev_lines
+    )
+  )
 }
