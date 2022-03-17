0a030ef7e334bcf1fd1fcbe5e10de9f35096e514
vincent <vincent@thinkr.fr>
2022-03-17 13:14:59 +0100

    test ok

--- tests/testthat/test-renv_stuff.R
+++ tests/testthat/test-renv_stuff.R
@@ -4,13 +4,12 @@ test_that("add_dockerfiles_renv all output file are present", {
   
   
   with_dir(pkg, {
-    for (fun in list(N
+    for (fun in list(
       add_dockerfile_with_renv,
       add_dockerfile_with_renv_shinyproxy
     )) {
 
-            # deploy_folder <-  file.path(tempdir(),make.names(paste0("deploy",round(runif(1,min = 0,max = 99999)))))
-            deploy_folder <-  file.path(tempdir(),"deploy2")
+            deploy_folder <-  file.path(tempdir(),make.names(paste0("deploy",round(runif(1,min = 0,max = 99999)))))
             fun(output_dir = deploy_folder)
 
           expect_exists(file.path(deploy_folder,"Dockerfile"))
@@ -18,7 +17,7 @@ test_that("add_dockerfiles_renv all output file are present", {
           expect_exists(file.path(deploy_folder,"README"))
           expect_exists(file.path(deploy_folder,"renv.lock.prod"))
           
-          list.files(path = deploy_folder,pattern = "tar.gz$")
+      expect_length(list.files(path = deploy_folder,pattern = "tar.gz$"),1)
           
 
     }
