09274a064988e56198e3420b4aef8ad98d5db959
colin <colin@thinkr.fr>
2022-04-08 08:48:10 +0200

    skip on windows

--- tests/testthat/test-pkg_tools.R
+++ tests/testthat/test-pkg_tools.R
@@ -2,6 +2,8 @@ test_that("pkgtools works", {
   withr::with_dir(pkg, {
     expect_equal(pkg_name(), fakename)
     expect_equal(pkg_version(), "0.0.0.9000")
+    # F-word windows path
+    skip_on_os("windows")
     expect_equal(pkg_path(), pkg)
   })
 })
