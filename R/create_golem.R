d6b5670a2a268b7bf0501678aefc16983bfed2e3
Colin Fay <contact@colinfay.me>
2022-07-02 10:28:48 +0200

    fix(config): unifying config manipulation
    All config manipulation now go through amend_golem_config.
    This allows to be sure that the !expr are not lost
    
    Close #709

--- R/create_golem.R
+++ R/create_golem.R
@@ -32,220 +32,220 @@
 #'
 #' @return The path, invisibly.
 create_golem <- function(
-  path,
-  check_name = TRUE,
-  open = TRUE,
-  overwrite = FALSE,
-  package_name = basename(path),
-  without_comments = FALSE,
-  project_hook = golem::project_hook,
-  with_git = FALSE,
-  ...
+	path,
+	check_name = TRUE,
+	open = TRUE,
+	overwrite = FALSE,
+	package_name = basename(path),
+	without_comments = FALSE,
+	project_hook = golem::project_hook,
+	with_git = FALSE,
+	...
 ) {
-  path <- normalizePath(path, mustWork = FALSE)
+	path <- normalizePath(path, mustWork = FALSE)
 
-  if (check_name) {
-    cat_rule("Checking package name")
-    getFromNamespace("check_package_name", "usethis")(package_name)
-    cat_green_tick("Valid package name")
-  }
-
-
-  if (dir.exists(path)) {
-    if (!isTRUE(overwrite)) {
-      stop(
-        paste(
-          "Project directory already exists. \n",
-          "Set `create_golem(overwrite = TRUE)` to overwrite anyway.\n",
-          "Be careful this will restore a brand new golem. \n",
-          "You might be at risk of losing your work !"
-        ),
-        call. = FALSE
-      )
-    } else {
-      cat_red_bullet("Overwriting existing project.")
-    }
-  } else {
-    cat_rule("Creating dir")
-    usethis::create_project(
-      path = path,
-      open = FALSE,
-    )
-    cat_green_tick("Created package directory")
-  }
+	if (check_name) {
+		cat_rule("Checking package name")
+		getFromNamespace("check_package_name", "usethis")(package_name)
+		cat_green_tick("Valid package name")
+	}
 
 
-  cat_rule("Copying package skeleton")
-  from <- golem_sys("shinyexample")
-
-  # Copy over whole directory
-  dir_copy(path = from, new_path = path, overwrite = TRUE)
-
-  # Listing copied files ***from source directory***
-  copied_files <- list.files(
-    path = from,
-    full.names = FALSE,
-    all.files = TRUE,
-    recursive = TRUE
-  )
-
-  # Going through copied files to replace package name
-  for (f in copied_files) {
-    copied_file <- file.path(path, f)
-
-    if (grepl("^REMOVEME", f)) {
-      file.rename(
-        from = copied_file,
-        to = file.path(path, gsub("REMOVEME", "", f))
-      )
-      copied_file <- file.path(path, gsub("REMOVEME", "", f))
-    }
-
-    if (!grepl("ico$", copied_file)) {
-      try(
-        {
-          replace_word(
-            file = copied_file,
-            pattern = "shinyexample",
-            replace = package_name
-          )
-        },
-        silent = TRUE
-      )
-    }
-  }
-
-  cat_green_tick("Copied app skeleton")
+	if (dir.exists(path)) {
+		if (!isTRUE(overwrite)) {
+			stop(
+				paste(
+					"Project directory already exists. \n",
+					"Set `create_golem(overwrite = TRUE)` to overwrite anyway.\n",
+					"Be careful this will restore a brand new golem. \n",
+					"You might be at risk of losing your work !"
+				),
+				call. = FALSE
+			)
+		} else {
+			cat_red_bullet("Overwriting existing project.")
+		}
+	} else {
+		cat_rule("Creating dir")
+		usethis::create_project(
+			path = path,
+			open = FALSE,
+		)
+		cat_green_tick("Created package directory")
+	}
 
 
-  cat_rule("Setting the default config")
+	cat_rule("Copying package skeleton")
+	from <- golem_sys("shinyexample")
 
-  yml_path <- file.path(path, "inst/golem-config.yml")
-  conf <- yaml::read_yaml(yml_path, eval.expr = TRUE)
-  yaml_golem_wd <- "here::here()"
-  attr(yaml_golem_wd, "tag") <- "!expr"
-  conf$dev$golem_wd <- yaml_golem_wd
-  conf$default$golem_name <- package_name
-  conf$default$golem_version <- "0.0.0.9000"
-  write_yaml(conf, yml_path)
+	# Copy over whole directory
+	dir_copy(path = from, new_path = path, overwrite = TRUE)
 
-  cat_green_tick("Configured app")
+	# Listing copied files ***from source directory***
+	copied_files <- list.files(
+		path = from,
+		full.names = FALSE,
+		all.files = TRUE,
+		recursive = TRUE
+	)
 
+	# Going through copied files to replace package name
+	for (f in copied_files) {
+		copied_file <- file.path(path, f)
 
-  cat_rule("Running project hook function")
+		if (grepl("^REMOVEME", f)) {
+			file.rename(
+				from = copied_file,
+				to = file.path(path, gsub("REMOVEME", "", f))
+			)
+			copied_file <- file.path(path, gsub("REMOVEME", "", f))
+		}
 
-  old <- setwd(path)
-  # TODO fix
-  # for some weird reason test() fails here when using golem::create_golem
-  # and I don't have time to search why rn
-  if (substitute(project_hook) == "golem::project_hook") {
-    project_hook <- getFromNamespace("project_hook", "golem")
-  }
-  project_hook(path = path, package_name = package_name, ...)
-  setwd(old)
+		if (!grepl("ico$", copied_file)) {
+			try(
+				{
+					replace_word(
+						file = copied_file,
+						pattern = "shinyexample",
+						replace = package_name
+					)
+				},
+				silent = TRUE
+			)
+		}
+	}
 
-  cat_green_tick("All set")
-
-
-  if (isTRUE(without_comments)) {
-    files <- list.files(
-      path = c(
-        file.path(path, "dev"),
-        file.path(path, "R")
-      ),
-      full.names = TRUE
-    )
-    for (file in files) {
-      remove_comments(file)
-    }
-  }
-
-
-  if (isTRUE(with_git)) {
-    cat_rule("Initializing git repository")
-    git_output <- system(
-      command = paste("git init", path),
-      ignore.stdout = TRUE,
-      ignore.stderr = TRUE
-    )
-    if (git_output) {
-      cat_red_bullet("Error initializing git epository")
-    } else {
-      cat_green_tick("Initialized git repository")
-    }
-  }
+	cat_green_tick("Copied app skeleton")
 
 
-  old <- setwd(path)
-  use_latest_dependencies()
+	cat_rule("Setting the default config")
 
-  # No .Rprofile for now
-  # cat_rule("Appending .Rprofile")
-  # write("# Sourcing user .Rprofile if it exists ", ".Rprofile", append = TRUE)
-  # write("home_profile <- file.path(", ".Rprofile", append = TRUE)
-  # write("  Sys.getenv(\"HOME\"), ", ".Rprofile", append = TRUE)
-  # write("  \".Rprofile\"", ".Rprofile", append = TRUE)
-  # write(")", ".Rprofile", append = TRUE)
-  # write("if (file.exists(home_profile)){", ".Rprofile", append = TRUE)
-  # write("  source(home_profile)", ".Rprofile", append = TRUE)
-  # write("}", ".Rprofile", append = TRUE)
-  # write("rm(home_profile)", ".Rprofile", append = TRUE)
-  #
-  # write("# Setting shiny.autoload.r to FALSE ", ".Rprofile", append = TRUE)
-  # write("options(shiny.autoload.r = FALSE)", ".Rprofile", append = TRUE)
-  # cat_green_tick("Appended")
+	yml_path <- file.path(path, "inst/golem-config.yml")
+	conf <- yaml::read_yaml(yml_path, eval.expr = TRUE)
+	yaml_golem_wd <- "golem::pkg_path()"
+	attr(yaml_golem_wd, "tag") <- "!expr"
+	conf$dev$golem_wd <- yaml_golem_wd
+	conf$default$golem_name <- package_name
+	conf$default$golem_version <- "0.0.0.9000"
+	write_yaml(conf, yml_path)
 
-  setwd(old)
+	cat_green_tick("Configured app")
 
 
-  cat_rule("Done")
-  cat_line(
-    paste0(
-      "A new golem named ",
-      package_name,
-      " was created at ",
-      normalizePath(path),
-      " .\n",
-      "To continue working on your app, start editing the 01_start.R file."
-    )
-  )
+	cat_rule("Running project hook function")
+
+	old <- setwd(path)
+	# TODO fix
+	# for some weird reason test() fails here when using golem::create_golem
+	# and I don't have time to search why rn
+	if (substitute(project_hook) == "golem::project_hook") {
+		project_hook <- getFromNamespace("project_hook", "golem")
+	}
+	project_hook(path = path, package_name = package_name, ...)
+	setwd(old)
+
+	cat_green_tick("All set")
 
 
-  if (isTRUE(open)) {
-    if (rstudioapi::isAvailable() & rstudioapi::hasFun("openProject")) {
-      rstudioapi::openProject(path = path)
-    } else {
-      setwd(path)
-    }
-  }
+	if (isTRUE(without_comments)) {
+		files <- list.files(
+			path = c(
+				file.path(path, "dev"),
+				file.path(path, "R")
+			),
+			full.names = TRUE
+		)
+		for (file in files) {
+			remove_comments(file)
+		}
+	}
 
 
-  return(
-    invisible(
-      normalizePath(path)
-    )
-  )
+	if (isTRUE(with_git)) {
+		cat_rule("Initializing git repository")
+		git_output <- system(
+			command = paste("git init", path),
+			ignore.stdout = TRUE,
+			ignore.stderr = TRUE
+		)
+		if (git_output) {
+			cat_red_bullet("Error initializing git epository")
+		} else {
+			cat_green_tick("Initialized git repository")
+		}
+	}
+
+
+	old <- setwd(path)
+	use_latest_dependencies()
+
+	# No .Rprofile for now
+	# cat_rule("Appending .Rprofile")
+	# write("# Sourcing user .Rprofile if it exists ", ".Rprofile", append = TRUE)
+	# write("home_profile <- file.path(", ".Rprofile", append = TRUE)
+	# write("  Sys.getenv(\"HOME\"), ", ".Rprofile", append = TRUE)
+	# write("  \".Rprofile\"", ".Rprofile", append = TRUE)
+	# write(")", ".Rprofile", append = TRUE)
+	# write("if (file.exists(home_profile)){", ".Rprofile", append = TRUE)
+	# write("  source(home_profile)", ".Rprofile", append = TRUE)
+	# write("}", ".Rprofile", append = TRUE)
+	# write("rm(home_profile)", ".Rprofile", append = TRUE)
+	#
+	# write("# Setting shiny.autoload.r to FALSE ", ".Rprofile", append = TRUE)
+	# write("options(shiny.autoload.r = FALSE)", ".Rprofile", append = TRUE)
+	# cat_green_tick("Appended")
+
+	setwd(old)
+
+
+	cat_rule("Done")
+	cat_line(
+		paste0(
+			"A new golem named ",
+			package_name,
+			" was created at ",
+			normalizePath(path),
+			" .\n",
+			"To continue working on your app, start editing the 01_start.R file."
+		)
+	)
+
+
+	if (isTRUE(open)) {
+		if (rstudioapi::isAvailable() & rstudioapi::hasFun("openProject")) {
+			rstudioapi::openProject(path = path)
+		} else {
+			setwd(path)
+		}
+	}
+
+
+	return(
+		invisible(
+			normalizePath(path)
+		)
+	)
 }
 
 # to be used in RStudio "new project" GUI
 create_golem_gui <- function(path, ...) {
-  dots <- list(...)
-  attempt::stop_if_not(
-    dots$project_hook,
-    ~ grepl("::", .x),
-    "{golem} project templates must be explicitely namespaced (pkg::fun)"
-  )
-  splt <- strsplit(dots$project_hook, "::")
-  project_hook <- getFromNamespace(
-    splt[[1]][2],
-    splt[[1]][1]
-  )
-  create_golem(
-    path = path,
-    open = FALSE,
-    without_comments = dots$without_comments,
-    project_hook = project_hook,
-    check_name = dots$check_name,
-    with_git = dots$with_git
-  )
+	dots <- list(...)
+	attempt::stop_if_not(
+		dots$project_hook,
+		~ grepl("::", .x),
+		"{golem} project templates must be explicitely namespaced (pkg::fun)"
+	)
+	splt <- strsplit(dots$project_hook, "::")
+	project_hook <- getFromNamespace(
+		splt[[1]][2],
+		splt[[1]][1]
+	)
+	create_golem(
+		path = path,
+		open = FALSE,
+		without_comments = dots$without_comments,
+		project_hook = project_hook,
+		check_name = dots$check_name,
+		with_git = dots$with_git
+	)
 }
--- R/options.R
+++ R/options.R
@@ -31,303 +31,273 @@
 #'
 #' @export
 #' @importFrom attempt stop_if_not
-#' @importFrom yaml read_yaml write_yaml
 #' @importFrom usethis proj_set
 #'
 #' @return Used for side-effects for the setters, and values from the
 #'     config in the getters.
 set_golem_options <- function(
-  golem_name = golem::pkg_name(),
-  golem_version = golem::pkg_version(),
-  golem_wd = golem::pkg_path(),
-  app_prod = FALSE,
-  talkative = TRUE
+	golem_name = golem::pkg_name(),
+	golem_version = golem::pkg_version(),
+	golem_wd = golem::pkg_path(),
+	app_prod = FALSE,
+	talkative = TRUE
 ) {
-  change_app_config_name(
-    name = golem_name,
-    path = golem_wd
-  )
 
-  cat_if_talk <- function(..., fun = cat_green_tick) {
-    if (talkative) {
-      fun(...)
-    }
-  }
+	# TODO here we'll run the
+	# golem_install_dev_pkg() function
 
-  conf_path <- get_current_config(
-    golem_wd,
-    set_options = FALSE
-  )
+	if (talkative) {
+		cli::cat_rule(
+			"Setting {golem} options in `golem-config.yml`"
+		)
+	}
 
-  stop_if(
-    conf_path,
-    is.null,
-    "Unable to retrieve golem config file."
-  )
+	# Let's start with wd
+	# Basically here the idea is to be able
+	# to keep the wd as an expr if it is the
+	# same as golem::pkg_path(), otherwise
+	# we use the explicit path
+	if (golem_wd == golem::pkg_path()) {
+		path <- "golem::pkg_path()"
+		attr(path, "tag") <- "!expr"
+	} else {
+		path <- golem_wd
+	}
 
-  cat_if_talk(
-    "Setting {golem} options in `golem-config.yml`",
-    fun = cli::cat_rule
-  )
+	set_golem_wd(
+		path = path,
+		talkative = talkative
+	)
 
-  conf <- read_yaml(conf_path, eval.expr = TRUE)
+	# Setting name of the golem
+	set_golem_name(
+		name = golem_name,
+		talkative = talkative
+	)
 
-  # Setting wd
-  if (golem_wd == here::here()) {
-    path <- "here::here()"
-    attr(path, "tag") <- "!expr"
-  } else {
-    path <- golem_wd
-  }
+	# Setting golem_version
+	set_golem_version(
+		version = golem_version,
+		talkative = talkative
+	)
 
-  cat_if_talk(
-    sprintf(
-      "Setting `golem_wd` to %s",
-      path
-    )
-  )
-  cat_if_talk(
-    "You can change golem working directory with set_golem_wd('path/to/wd')",
-    fun = cat_line
-  )
+	# Setting app_prod
+	set_golem_things(
+		"app_prod",
+		app_prod,
+		path = golem_wd,
+		talkative = talkative
+	)
 
-  conf$dev$golem_wd <- path
+	# This part is for {usethis} and {here}
+	if (talkative) {
+		cli::cat_rule(
+			"Setting {usethis} project as `golem_wd`"
+		)
+	}
 
-  # Setting name of the golem
-  cat_if_talk(
-    sprintf(
-      "Setting `golem_name` to %s",
-      golem_name
-    )
-  )
-  conf$default$golem_name <- golem_name
-
-  # Setting golem_version
-  cat_if_talk(
-    sprintf(
-      "Setting `golem_version` to %s",
-      golem_version
-    )
-  )
-  conf$default$golem_version <- as.character(golem_version)
-
-  # Setting app_prod
-  cat_if_talk(
-    sprintf(
-      "Setting `app_prod` to %s",
-      app_prod
-    )
-  )
-  conf$default$app_prod <- app_prod
-
-  # Export
-  write_yaml(
-    conf,
-    conf_path
-  )
-
-  cat_if_talk(
-    "Setting {usethis} project as `golem_wd`",
-    fun = cli::cat_rule
-  )
-  proj_set(golem_wd)
+	proj_set(golem_wd)
 }
 
-#' @importFrom yaml read_yaml write_yaml
 set_golem_things <- function(
-  key,
-  value,
-  path,
-  talkative,
-  config = "default"
+	key,
+	value,
+	path,
+	talkative,
+	config = "default"
 ) {
-  conf_path <- get_current_config(path, set_options = FALSE)
-  stop_if(
-    conf_path,
-    is.null,
-    "Unable to retrieve golem config file."
-  )
-  cat_if_talk <- function(..., fun = cat_green_tick) {
-    if (talkative) {
-      fun(...)
-    }
-  }
+	conf_path <- get_current_config(path, set_options = FALSE)
+	stop_if(
+		conf_path,
+		is.null,
+		"Unable to retrieve golem config file."
+	)
+	cat_if_talk <- function(..., fun = cat_green_tick) {
+		if (talkative) {
+			fun(...)
+		}
+	}
 
-  cat_if_talk(
-    sprintf(
-      "Setting `%s` to %s",
-      key,
-      value
-    )
-  )
+	cat_if_talk(
+		sprintf(
+			"Setting `%s` to %s",
+			key,
+			value
+		)
+	)
 
-  conf <- read_yaml(conf_path, eval.expr = TRUE)
-  conf[[config]][[key]] <- value
-  write_yaml(
-    conf,
-    conf_path
-  )
+	if (key == "golem_wd") {
+		cat_if_talk(
+			"You can change golem working directory with set_golem_wd('path/to/wd')",
+			fun = cat_line
+		)
+	}
 
-  invisible(path)
+	amend_golem_config(
+		key = key,
+		value = value,
+		config = config
+	)
+
+	invisible(path)
 }
 
 #' @export
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_wd <- function(
-  path = golem::pkg_path(),
-  talkative = TRUE
+	path = golem::pkg_path(),
+	talkative = TRUE
 ) {
-  path <- path_abs(path)
-  # Setting wd
+	path <- path_abs(path)
 
-  if (path == here::here()) {
-    path <- "here::here()"
-    attr(path, "tag") <- "!expr"
-  }
+	if (path == golem::pkg_path()) {
+		path <- "golem::pkg_path()"
+		attr(path, "tag") <- "!expr"
+	}
 
-  set_golem_things(
-    "golem_wd",
-    path,
-    path,
-    talkative = talkative,
-    config = "dev"
-  )
+	set_golem_things(
+		"golem_wd",
+		path,
+		path,
+		talkative = talkative,
+		config = "dev"
+	)
 
-  invisible(path)
+	invisible(path)
 }
 
 #' @export
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_name <- function(
-  name = golem::pkg_name(),
-  path = golem::pkg_path(),
-  talkative = TRUE
+	name = golem::pkg_name(),
+	path = golem::pkg_path(),
+	talkative = TRUE
 ) {
-  path <- path_abs(path)
-  # Changing in YAML
-  set_golem_things(
-    "golem_name",
-    name,
-    path,
-    talkative = talkative
-  )
-  # Changing in app-config.R
-  change_app_config_name(
-    name = name,
-    path = path
-  )
+	path <- path_abs(path)
+	# Changing in YAML
+	set_golem_things(
+		"golem_name",
+		name,
+		path,
+		talkative = talkative
+	)
+	# Changing in app-config.R
+	change_app_config_name(
+		name = name,
+		path = path
+	)
 
-  # Changing in DESCRIPTION
-  desc <- desc::description$new(
-    file = fs::path(
-      path,
-      "DESCRIPTION"
-    )
-  )
-  desc$set(
-    Package = name
-  )
-  desc$write(
-    file = "DESCRIPTION"
-  )
+	# Changing in DESCRIPTION
+	desc <- desc::description$new(
+		file = fs::path(
+			path,
+			"DESCRIPTION"
+		)
+	)
+	desc$set(
+		Package = name
+	)
+	desc$write(
+		file = "DESCRIPTION"
+	)
 
-  invisible(name)
+	invisible(name)
 }
 
 #' @export
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_version <- function(
-  version = golem::pkg_version(),
-  path = golem::pkg_path(),
-  talkative = TRUE
+	version = golem::pkg_version(),
+	path = golem::pkg_path(),
+	talkative = TRUE
 ) {
-  path <- path_abs(path)
-  set_golem_things(
-    "golem_version",
-    as.character(version),
-    path,
-    talkative = talkative
-  )
-  desc <- desc::description$new(file = fs::path(path, "DESCRIPTION"))
-  desc$set_version(
-    version = version
-  )
-  desc$write(
-    file = "DESCRIPTION"
-  )
+	path <- path_abs(path)
+	set_golem_things(
+		"golem_version",
+		as.character(version),
+		path,
+		talkative = talkative
+	)
+	desc <- desc::description$new(file = fs::path(path, "DESCRIPTION"))
+	desc$set_version(
+		version = version
+	)
+	desc$write(
+		file = "DESCRIPTION"
+	)
 
-  invisible(version)
+	invisible(version)
 }
 
 #' @importFrom config get
 get_golem_things <- function(
-  value,
-  config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
-  use_parent = TRUE,
-  path
+	value,
+	config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
+	use_parent = TRUE,
+	path
 ) {
-  conf_path <- get_current_config(path, set_options = TRUE)
-  stop_if(
-    conf_path,
-    is.null,
-    "Unable to retrieve golem config file."
-  )
-  config::get(
-    value = value,
-    config = config,
-    file = conf_path,
-    use_parent = TRUE
-  )
+	conf_path <- get_current_config(path, set_options = TRUE)
+	stop_if(
+		conf_path,
+		is.null,
+		"Unable to retrieve golem config file."
+	)
+	config::get(
+		value = value,
+		config = config,
+		file = conf_path,
+		use_parent = TRUE
+	)
 }
 
 
 #' @export
 #' @rdname golem_opts
 get_golem_wd <- function(
-  use_parent = TRUE,
-  path = golem::pkg_path()
+	use_parent = TRUE,
+	path = golem::pkg_path()
 ) {
-  get_golem_things(
-    value = "golem_wd",
-    config = "dev",
-    use_parent = use_parent,
-    path = path
-  )
+	get_golem_things(
+		value = "golem_wd",
+		config = "dev",
+		use_parent = use_parent,
+		path = path
+	)
 }
 
 #' @export
 #' @rdname golem_opts
 get_golem_name <- function(
-  config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
-  use_parent = TRUE,
-  path = golem::pkg_path()
+	config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
+	use_parent = TRUE,
+	path = golem::pkg_path()
 ) {
-  nm <- get_golem_things(
-    value = "golem_name",
-    config = config,
-    use_parent = use_parent,
-    path = path
-  )
-  if (is.null(nm)) {
-    nm <- golem::pkg_name()
-  }
-  nm
+	nm <- get_golem_things(
+		value = "golem_name",
+		config = config,
+		use_parent = use_parent,
+		path = path
+	)
+	if (is.null(nm)) {
+		nm <- golem::pkg_name()
+	}
+	nm
 }
 
 #' @export
 #' @rdname golem_opts
 get_golem_version <- function(
-  config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
-  use_parent = TRUE,
-  path = golem::pkg_path()
+	config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
+	use_parent = TRUE,
+	path = golem::pkg_path()
 ) {
-  get_golem_things(
-    value = "golem_version",
-    config = config,
-    use_parent = use_parent,
-    path = path
-  )
+	get_golem_things(
+		value = "golem_version",
+		config = config,
+		use_parent = use_parent,
+		path = path
+	)
 }
