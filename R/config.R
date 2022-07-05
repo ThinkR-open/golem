e5df61252983e20b66564ee6acbf906e50575e57
Colin Fay <contact@colinfay.me>
2022-07-05 17:36:40 +0200

    feat(config): get_current_config no longer set options

--- R/config.R
+++ R/config.R
@@ -51,11 +51,18 @@ guess_where_config <- function(
 	return(NULL)
 }
 
+#' Get the path to the current config File
+#'
+#' This function tries to guess where the golem-config file is located.
+#' If it can't find it, this function asks the
+#' user if they want to set the golem skeleton.
+#'
 #' @importFrom fs file_copy path
-get_current_config <- function(
-	path = ".",
-	set_options = TRUE
-) {
+#'
+#' @param path Path to start looking for the config
+#'
+#' @export
+get_current_config <- function(path = ".") {
 
 	# We check wether we can guess where the config file is
 	path_conf <- guess_where_config(path)
@@ -103,9 +110,7 @@ get_current_config <- function(
 				"shinyexample",
 				golem::pkg_name()
 			)
-			if (set_options) {
-				set_golem_options()
-			}
+			# TODO This should also create the dev folder
 		} else {
 			stop(
 				sprintf(
@@ -140,80 +145,4 @@ change_app_config_name <- function(
 		name
 	)
 	write(app_config, pth)
-}
-
-
-# find and tag expressions in a yaml
-# used internally in `amend_golem_config`
-#' @importFrom utils modifyList
-find_and_tag_exprs <- function(conf_path) {
-	conf <- yaml::read_yaml(conf_path, eval.expr = FALSE)
-	conf.eval <- yaml::read_yaml(conf_path, eval.expr = TRUE)
-
-	expr_list <- lapply(names(conf), function(x) {
-		conf[[x]][!conf[[x]] %in% conf.eval[[x]]]
-	})
-	names(expr_list) <- names(conf)
-	expr_list <- Filter(function(x) length(x) > 0, expr_list)
-	add_expr_tag <- function(tag) {
-		attr(tag[[1]], "tag") <- "!expr"
-		tag
-	}
-	tagged_exprs <- lapply(expr_list, add_expr_tag)
-	modifyList(conf, tagged_exprs)
-}
-
-#' Amend golem config file
-#'
-#' @param key key of the value to add in `config`
-#' @inheritParams config::get
-#' @inheritParams add_module
-#' @inheritParams set_golem_options
-#'
-#' @export
-#' @importFrom yaml read_yaml write_yaml
-#' @importFrom attempt stop_if
-#'
-#' @return Used for side effects.
-amend_golem_config <- function(
-	key,
-	value,
-	config = "default",
-	pkg = get_golem_wd(),
-	talkative = TRUE
-) {
-	conf_path <- get_current_config(pkg)
-	stop_if(
-		conf_path,
-		is.null,
-		"Unable to retrieve golem config file."
-	)
-	cat_if_talk <- function(..., fun = cat_green_tick) {
-		if (talkative) {
-			fun(...)
-		}
-	}
-
-	cat_if_talk(
-		sprintf(
-			"Setting `%s` to %s",
-			key,
-			value
-		)
-	)
-
-	if (key == "golem_wd") {
-		cat_if_talk(
-			"You can change golem working directory with set_golem_wd('path/to/wd')",
-			fun = cat_line
-		)
-	}
-
-	conf <- find_and_tag_exprs(conf_path)
-	conf[[config]][[key]] <- value
-	write_yaml(
-		conf,
-		conf_path
-	)
-	invisible(TRUE)
 }
