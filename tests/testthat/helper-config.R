e166a8885041d7f0f25f06cdb66a1ec8ce3b2ef6
vincent <vincent@thinkr.fr>
2022-07-08 10:19:33 +0200

    fix unit test ( version bourin )

--- tests/testthat/helper-config.R
+++ tests/testthat/helper-config.R
@@ -55,22 +55,50 @@ fakename <- sprintf(
   paste0(sample(safe_let(), 10, TRUE), collapse = ""),
   gsub("[ :-]", "", Sys.time())
 )
+fakename2 <- sprintf(
+  "%s%s",
+  paste0(sample(safe_let(), 10, TRUE), collapse = ""),
+  gsub("[ :-]", "", Sys.time())
+)
+
+
+
+
+## random dir
+randir <- paste0(sample(safe_let(), 10, TRUE), collapse = "")
 
 tpdir <- normalizePath(tempdir())
 unlink(file.path(tpdir, fakename), recursive = TRUE)
 create_golem(file.path(tpdir, fakename), open = FALSE)
 pkg <- file.path(tpdir, fakename)
 
-## random dir
-randir <- paste0(sample(safe_let(), 10, TRUE), collapse = "")
+
+
 fp <- file.path("inst/app", randir)
 dir.create(file.path(pkg, fp), recursive = TRUE)
+
+
+
+tpdir2 <- normalizePath(tempdir())
+unlink(file.path(tpdir2, fakename2), recursive = TRUE)
+create_golem(file.path(tpdir2, fakename2), open = FALSE)
+pkg2 <- file.path(tpdir2, fakename2)
+fp2 <- file.path("inst/app", randir)
+dir.create(file.path(pkg, fp2), recursive = TRUE)
+
+
 
 rand_name <- function() {
   paste0(sample(safe_let(), 10, TRUE), collapse = "")
 }
 
 withr::with_dir(pkg, {
+  set_golem_options()
+  usethis::proj_set(pkg)
+  orig_test <- set_golem_wd(pkg)
+  usethis::use_mit_license("Golem")
+})
+withr::with_dir(pkg2, {
   set_golem_options()
   usethis::proj_set(pkg)
   orig_test <- set_golem_wd(pkg)
--- tests/testthat/test-renv_stuff.R
+++ tests/testthat/test-renv_stuff.R
@@ -1,14 +1,12 @@
 test_that("add_dockerfiles_renv all output file are present", {
-  skip_if_not_installed("dockerfiler", "0.1.5.0002")
-  
-  
-  
+  skip_if_not_installed("dockerfiler", "0.2.0")
+
   with_dir(pkg, {
     for (fun in list(
-      add_dockerfile_with_renv,
-      add_dockerfile_with_renv_shinyproxy
+      add_dockerfile_with_renv#,
+      # add_dockerfile_with_renv
     )) {
-
+      
             deploy_folder <-  file.path(tempdir(),make.names(paste0("deploy",round(runif(1,min = 0,max = 99999)))))
             fun(output_dir = deploy_folder)
 
@@ -16,10 +14,35 @@ test_that("add_dockerfiles_renv all output file are present", {
           expect_exists(file.path(deploy_folder,"Dockerfile_socle"))
           expect_exists(file.path(deploy_folder,"README"))
           expect_exists(file.path(deploy_folder,"renv.lock.prod"))
-          
+
       expect_length(list.files(path = deploy_folder,pattern = "tar.gz$"),1)
-          
+unlink(deploy_folder,force = TRUE,recursive = TRUE)
 
     }
   })
+
+
+})
+test_that("add_dockerfile_with_renv_shinyproxy all output file are present", {
+  skip_if_not_installed("dockerfiler", "0.2.0")
+  
+  with_dir(pkg2, {
+    for (fun in list(
+      add_dockerfile_with_renv
+    )) {
+            deploy_folder <-  file.path(tempdir(),make.names(paste0("deploy",round(runif(1,min = 0,max = 99999)))))
+            fun(output_dir = deploy_folder)
+
+          expect_exists(file.path(deploy_folder,"Dockerfile"))
+          expect_exists(file.path(deploy_folder,"Dockerfile_socle"))
+          expect_exists(file.path(deploy_folder,"README"))
+          expect_exists(file.path(deploy_folder,"renv.lock.prod"))
+
+      expect_length(list.files(path = deploy_folder,pattern = "tar.gz$"),1)
+
+
+    }
+  })
+
+
 })
