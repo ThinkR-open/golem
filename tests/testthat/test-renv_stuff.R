531352b27b349e3f9beb6f104cfa39b9c0c247c6
colin <colin@thinkr.fr>
2022-07-12 22:22:53 +0200

    test: Added add_dockerfile_with_renv_heroku + prevent from opening
    + Added add_dockerfile_with_renv_heroku to the test suite
    
    + Prevent the dockerfile created to be open when running the tests
    
    + Styling

--- tests/testthat/test-renv_stuff.R
+++ tests/testthat/test-renv_stuff.R
@@ -4,22 +4,30 @@ test_that("add_dockerfiles_renv and add_dockerfile_with_renv_shinyproxy all outp
   with_dir(pkg, {
     for (fun in list(
       add_dockerfile_with_renv,
-      add_dockerfile_with_renv_shinyproxy
+      add_dockerfile_with_renv_shinyproxy,
+      add_dockerfile_with_renv_heroku
     )) {
-      
-            deploy_folder <-  file.path(tempdir(),make.names(paste0("deploy",round(runif(1,min = 0,max = 99999)))))
-            fun(output_dir = deploy_folder)
+      deploy_folder <- file.path(
+        tempdir(),
+        make.names(
+          paste0(
+            "deploy",
+            round(
+              runif(1, min = 0, max = 99999)
+            )
+          )
+        )
+      )
 
-          expect_exists(file.path(deploy_folder,"Dockerfile"))
-          expect_exists(file.path(deploy_folder,"Dockerfile_base"))
-          expect_exists(file.path(deploy_folder,"README"))
-          expect_exists(file.path(deploy_folder,"renv.lock.prod"))
+      fun(output_dir = deploy_folder, open = FALSE)
 
-      expect_length(list.files(path = deploy_folder,pattern = "tar.gz$"),1)
-      unlink(deploy_folder,force = TRUE,recursive = TRUE)
+      expect_exists(file.path(deploy_folder, "Dockerfile"))
+      expect_exists(file.path(deploy_folder, "Dockerfile_base"))
+      expect_exists(file.path(deploy_folder, "README"))
+      expect_exists(file.path(deploy_folder, "renv.lock.prod"))
 
+      expect_length(list.files(path = deploy_folder, pattern = "tar.gz$"), 1)
+      unlink(deploy_folder, force = TRUE, recursive = TRUE)
     }
   })
-
-
 })
