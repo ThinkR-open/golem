75ee80706659dbcbca28d4c0eac8e734370441e0
colin <colin@thinkr.fr>
2022-08-08 22:01:49 +0200

    chore: changed indentatino

--- R/golem-yaml-set.R
+++ R/golem-yaml-set.R
@@ -30,101 +30,101 @@
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_wd <- function(
-	golem_wd = golem::pkg_path(),
-	pkg = golem::pkg_path(),
-	talkative = TRUE
+  golem_wd = golem::pkg_path(),
+  pkg = golem::pkg_path(),
+  talkative = TRUE
 ) {
-	if (
-		golem_wd == "golem::pkg_path()" |
-			golem_wd == golem::pkg_path()
-	) {
-		golem_yaml_path <- "golem::pkg_path()"
-		attr(golem_yaml_path, "tag") <- "!expr"
-	} else {
-		golem_yaml_path <- path_abs(golem_wd)
-	}
+  if (
+    golem_wd == "golem::pkg_path()" |
+      golem_wd == golem::pkg_path()
+  ) {
+    golem_yaml_path <- "golem::pkg_path()"
+    attr(golem_yaml_path, "tag") <- "!expr"
+  } else {
+    golem_yaml_path <- path_abs(golem_wd)
+  }
 
-	amend_golem_config(
-		key = "golem_wd",
-		value = golem_yaml_path,
-		config = "dev",
-		pkg = pkg,
-		talkative = talkative
-	)
+  amend_golem_config(
+    key = "golem_wd",
+    value = golem_yaml_path,
+    config = "dev",
+    pkg = pkg,
+    talkative = talkative
+  )
 
-	invisible(path)
+  invisible(path)
 }
 
 #' @export
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_name <- function(
-	name = golem::pkg_name(),
-	pkg = golem::pkg_path(),
-	talkative = TRUE
+  name = golem::pkg_name(),
+  pkg = golem::pkg_path(),
+  talkative = TRUE
 ) {
-	# Changing in YAML
-	amend_golem_config(
-		key = "golem_name",
-		value = name,
-		config = "default",
-		pkg = pkg,
-		talkative = talkative
-	)
-	# Changing in app-config.R
-	change_app_config_name(
-		name = name,
-		path = pkg
-	)
+  # Changing in YAML
+  amend_golem_config(
+    key = "golem_name",
+    value = name,
+    config = "default",
+    pkg = pkg,
+    talkative = talkative
+  )
+  # Changing in app-config.R
+  change_app_config_name(
+    name = name,
+    path = pkg
+  )
 
-	# Changing in DESCRIPTION
-	desc <- desc::description$new(
-		file = fs::path(
-			path,
-			"DESCRIPTION"
-		)
-	)
-	desc$set(
-		Package = name
-	)
-	desc$write(
-		file = "DESCRIPTION"
-	)
+  # Changing in DESCRIPTION
+  desc <- desc::description$new(
+    file = fs::path(
+      path,
+      "DESCRIPTION"
+    )
+  )
+  desc$set(
+    Package = name
+  )
+  desc$write(
+    file = "DESCRIPTION"
+  )
 
-	invisible(name)
+  invisible(name)
 }
 
 #' @export
 #' @rdname golem_opts
 #' @importFrom fs path_abs
 set_golem_version <- function(
-	version = golem::pkg_version(),
-	pkg = golem::pkg_path(),
-	talkative = TRUE
+  version = golem::pkg_version(),
+  pkg = golem::pkg_path(),
+  talkative = TRUE
 ) {
-	path <- path_abs(path)
+  path <- path_abs(path)
 
-	# Changing in YAML
-	amend_golem_config(
-		key = "golem_version",
-		value = as.character(version),
-		config = "default",
-		pkg = pkg,
-		talkative = talkative
-	)
+  # Changing in YAML
+  amend_golem_config(
+    key = "golem_version",
+    value = as.character(version),
+    config = "default",
+    pkg = pkg,
+    talkative = talkative
+  )
 
-	desc <- desc::description$new(
-		file = fs::path(
-			pkg,
-			"DESCRIPTION"
-		)
-	)
-	desc$set_version(
-		version = version
-	)
-	desc$write(
-		file = "DESCRIPTION"
-	)
+  desc <- desc::description$new(
+    file = fs::path(
+      pkg,
+      "DESCRIPTION"
+    )
+  )
+  desc$set_version(
+    version = version
+  )
+  desc$write(
+    file = "DESCRIPTION"
+  )
 
-	invisible(version)
+  invisible(version)
 }
