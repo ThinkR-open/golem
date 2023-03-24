f73055021f66ed09a5019f645d4b28cdb97a20c8
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-24 19:08:20 +0100

    Fix 987

--- R/modules_fn.R
+++ R/modules_fn.R
@@ -243,8 +243,8 @@ module_template <- function(
     } else {
       write_there("#' @noRd ")
     }
-    write_there(sprintf("mod_%s_server <- function(id) {", name))
-    write_there("  moduleServer(id, function(input, output, session) {")
+    write_there(sprintf("mod_%s_server <- function(id){", name))
+    write_there("  moduleServer(id, function(input, output, session){")
     write_there("    ns <- session$ns")
     write_there(ph_server)
     write_there("  })")
