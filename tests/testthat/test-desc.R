0a529cafbd8db0b70d0ad387cd4a92a1d41e0844
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-05 15:07:13 +0200

    refactor: update tests for fill_desc()
    - changes since values of authors are of type person() which
        requires parsing the entries

--- tests/testthat/test-desc.R
+++ tests/testthat/test-desc.R
@@ -10,9 +10,11 @@ test_that("desc works", {
             pkg_name = fakename,
             pkg_title = "newtitle",
             pkg_description = "Newdescription.",
-            author_first_name = "firstname",
-            author_last_name = "lastname",
-            author_email = "name@test.com",
+            authors = person(
+              given = "firstname",
+              family = "lastname",
+              email = "name@test.com"
+            ),
             repo_url = "http://repo_url.com",
             pkg_version = "0.0.0.9000"
           )
@@ -23,9 +25,7 @@ test_that("desc works", {
       fakename,
       "newtitle",
       "Newdescription.",
-      "firstname",
-      "lastname",
-      "name@test.com",
+      "person('firstname', 'lastname', , 'name@test.com')",
       "http://repo_url.com",
       "0.0.0.9000"
     )
@@ -34,13 +34,19 @@ test_that("desc works", {
     expect_true(
       all(
         as.logical(lapply(
-          add_desc,
+          add_desc[-4],
           function(x) {
             any(grepl(x, desc))
           }
         ))
       )
     )
+    # add additional test, as authors = person(...) requires parsing test
+    tmp_test_add_desc <- eval(parse(text = add_desc[4]))
+    tmp_test_desc <- eval(parse(text = desc[[5]]))
+    expect_identical(
+      tmp_test_add_desc,
+      tmp_test_desc)
 
     expect_true(
       stringr::str_detect(output, "DESCRIPTION file modified")
