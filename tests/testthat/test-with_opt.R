1b833327d016d0196b8b2ad715b9da82cd9a19ce
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-07-07 14:39:26 +0200

    tests: add tests to further increase coverage
    Add a test for get_golem_options() function which was untested before.

--- tests/testthat/test-with_opt.R
+++ tests/testthat/test-with_opt.R
@@ -35,62 +35,98 @@ test_that("maintenance page works directly and via with_golem_options()", {
     }
   )
 })
-test_that("with_golem_options() disables 'print'-flag on Posit platforms", {
-  path_dummy_golem <- tempfile(pattern = "dummygolem")
-  create_golem(
-    path = path_dummy_golem,
-    open = FALSE
-  )
-  with_dir(
-    path_dummy_golem,
-    {
-    # generate a run_app file with 'print = TRUE'
-    # run_app_with_print_FALSE <- readLines("R/run_app.R")
-    # last_lines <- run_app_with_print_FALSE[26:28]
-    # last_lines[1] <- "    golem_opts = list(...),"
-    # run_app_with_print_TRUE <- c(
-    #   run_app_with_print_FALSE[-c(26:28)],
-    #   last_lines[1],
-    #   "    print = FALSE,", last_lines[2:3])
-    # writeLines(run_app_with_print_TRUE, "R/run_app.R")
-    # source("R/run_app.R")
-    app_print_true <- run_app()
-    # generate an app with print = FALSE by setting the port
-    Sys.setenv("SHINY_PORT" = "123")
-    app_with_port <- run_app()
-    expect_true(
-      isTRUE(
-        all.equal(
+test_that(
+  "with_golem_options() disables 'print'-flag on Posit platforms",
+  {
+    path_dummy_golem <- tempfile(pattern = "dummygolem")
+    create_golem(
+      path = path_dummy_golem,
+      open = FALSE
+    )
+    with_dir(
+      path_dummy_golem,
+      {
+        # generate a run_app file with 'print = TRUE'
+        # run_app_with_print_FALSE <- readLines("R/run_app.R")
+        # last_lines <- run_app_with_print_FALSE[26:28]
+        # last_lines[1] <- "    golem_opts = list(...),"
+        # run_app_with_print_TRUE <- c(
+        #   run_app_with_print_FALSE[-c(26:28)],
+        #   last_lines[1],
+        #   "    print = FALSE,", last_lines[2:3])
+        # writeLines(run_app_with_print_TRUE, "R/run_app.R")
+        # source("R/run_app.R")
+        app_print_true <- run_app()
+        # generate an app with print = FALSE by setting the port
+        Sys.setenv("SHINY_PORT" = "123")
+        app_with_port <- run_app()
+        expect_true(
+          isTRUE(
+            all.equal(
+              app_print_true,
+              app_with_port
+            )
+          )
+        )
+        # # generate an app with print = TRUE by unsetting the port
+        Sys.setenv("SHINY_PORT" = "")
+        app_with_port <- run_app()
+        expect_equal(
           app_print_true,
           app_with_port
         )
-      )
+      }
     )
-    # # generate an app with print = TRUE by unsetting the port
-    Sys.setenv("SHINY_PORT" = "")
-    app_with_port <- run_app()
-    expect_equal(
-      app_print_true,
-      app_with_port
-    )
-    }
-  )
   }
 )
-# test_that("with_golem_options() disables 'print'-flag on Posit platforms", {
-#   path_dummy_golem <- tempfile(pattern = "dummygolem")
-#   create_golem(
-#     path = path_dummy_golem,
-#     open = FALSE
-#   )
-#   with_dir(
-#     path_dummy_golem,
-#     {
-#       gol_opt_default <- get_golem_options()
-#       gol_opt_set     <- get_golem_options()
-#       expect_equal(gol_opt_default, gol_opt_set)
-#       options(golem.app.prod = FALSE)
-#     }
-#   )
-#   }
-# )
+test_that(
+  "get_golem_options() retrieves 'golem_options'",
+  {
+    path_dummy_golem <- tempfile(pattern = "dummygolem")
+    create_golem(
+      path = path_dummy_golem,
+      open = FALSE
+    )
+    with_dir(
+      path_dummy_golem,
+      {
+        # 0. Preparation:
+        # 0.A backup hidden variable '.global' from the shiny pkg-namespace
+        tmp_globals_backup <- getFromNamespace(".globals", "shiny")
+        # 0.B make a full clone of this variable (which is an environment)
+        tmp_globals_override <- as.environment(
+          as.list(
+            tmp_globals_backup,
+            all.names = TRUE
+          )
+        )
+        # 0.C mimick the existence of "golem_options" inside the clone
+        tmp_globals_override$options$golem_options$country_01 <- "france"
+        tmp_globals_override$options$golem_options$country_02 <- "germany"
+        # I Testing:
+        # I.A temporarily override ".globals" with the clone
+        assignInNamespace(".globals", tmp_globals_override, ns = "shiny")
+        # I.B test that get_golem_options() correctly retrieves "golem_options"
+        expect_equal(
+          get_golem_options(),
+          list(
+            country_01 = "france",
+            country_02 = "germany"
+          )
+        )
+        expect_equal(
+          get_golem_options("country_01"),
+          "france"
+        )
+        expect_equal(
+          get_golem_options("country_02"),
+          "germany"
+        )
+        # II. assign global variable back and check that this reset is successful
+        assignInNamespace(".globals", tmp_globals_backup, ns = "shiny")
+        expect_null(getShinyOption("golem_options"))
+        expect_null(get_golem_options())
+      }
+    )
+  }
+)
