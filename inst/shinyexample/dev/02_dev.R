bdafdbdda41406459a92003ad0da0b9d799c978b
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-12-01 12:29:34 +0100

    fix: remove deprecated usethis funcs

--- inst/shinyexample/dev/02_dev.R
+++ inst/shinyexample/dev/02_dev.R
@@ -74,14 +74,6 @@ usethis::use_github_action_check_full()
 # Add action for PR
 usethis::use_github_action_pr_commands()
 
-# Travis CI
-usethis::use_travis()
-usethis::use_travis_badge()
-
-# AppVeyor
-usethis::use_appveyor()
-usethis::use_appveyor_badge()
-
 # Circle CI
 usethis::use_circleci()
 usethis::use_circleci_badge()
