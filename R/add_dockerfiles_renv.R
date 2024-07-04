a5569e92f74e2963451e3f779ef5ed0b423d5973
Vincent Guyader <vincent@thinkr.fr>
2024-07-04 06:48:46 +0000

    feat It is now easier to modify the renv.config.pak.enabled parameter in the Dockerfile generated by golem::add_dockerfile_with_renv_*().

--- R/add_dockerfiles_renv.R
+++ R/add_dockerfiles_renv.R
@@ -83,7 +83,7 @@ add_dockerfile_with_renv_ <- function(
 
   my_dock$COPY(basename(lockfile), "renv.lock")
 
-  my_dock$RUN("R -e 'renv::restore()'")
+  my_dock$RUN("R -e 'options(renv.config.pak.enabled = FALSE);renv::restore()'")
 
   if (update_tar_gz) {
     old_version <- list.files(path = output_dir, pattern = paste0(golem::get_golem_name(), "_*.*.tar.gz"), full.names = TRUE)
