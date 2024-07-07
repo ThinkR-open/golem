a9a0f64dc1eab91c25edd58d77aa9f8f51cc28bf
colin <colin@thinkr.fr>
2024-07-07 18:41:42 +0200

    test: simple test for test helpers

--- tests/testthat/test-test_helpers.R
+++ tests/testthat/test-test_helpers.R
@@ -1,23 +1,25 @@
 test_that("test expect_shinytag", {
-  with_dir(pkg, {
-    withr::with_options(
-      c("golem.quiet" = FALSE),
-      {
-        expect_equal(capture_output(expect_shinytag(favicon("jean"))), "")
-        expect_error(expect_shinytag("pierre"))
-      }
-    )
-  })
+  expect_equal(
+    capture_output(
+      expect_shinytag(favicon("jean"))
+    ),
+    ""
+  )
+  expect_error(
+    expect_shinytag("pierre")
+  )
 })
 
 test_that("test expect_shinytaglist", {
-  with_dir(pkg, {
-    withr::with_options(
-      c("golem.quiet" = FALSE),
-      {
-        expect_equal(capture_output(expect_shinytaglist(shiny::tagList())), "")
-        expect_error(expect_shinytaglist("test"))
-      }
-    )
-  })
+  expect_equal(
+    capture_output(
+      expect_shinytaglist(
+        shiny::tagList()
+      )
+    ),
+    ""
+  )
+  expect_error(
+    expect_shinytaglist("test")
+  )
 })
