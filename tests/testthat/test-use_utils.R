268e0b08fb940e89f1beb3ac86ba6bdffd975e2e
colin <colin@thinkr.fr>
2024-07-07 17:41:48 +0200

    test: fully covering use_utils

--- tests/testthat/test-use_utils.R
+++ tests/testthat/test-use_utils.R
@@ -53,7 +53,51 @@ test_that("use_utils_ui works", {
       )
     }
   )
+  # Testint that the function is
+  # indempotent by running it twice
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_ui(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
 
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_ui.R"
+    )
+  )
+  expect_exists(
+    file.path(
+      pkg,
+      "tests/testthat/test-golem_utils_ui.R"
+    )
+  )
+  # We test that we can still add the tests even if the utils ui
+  # is already there
+  remove_file(
+    file.path(
+      pkg,
+      "R/golem_utils_ui.R"
+    )
+  )
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_ui(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
   expect_exists(
     file.path(
       pkg,
@@ -114,6 +158,49 @@ test_that("use_utils_ui works", {
       recursive = TRUE
     )
   })
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_server(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
+  # Testint that the function is
+  # indempotent by running it twice
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_server(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
+
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_server.R"
+    )
+  )
+  expect_exists(
+    file.path(
+      pkg,
+      "tests/testthat/test-golem_utils_server.R"
+    )
+  )
+  remove_file(
+    file.path(
+      pkg,
+      "R/golem_utils_server.R"
+    )
+  )
   withr::with_options(
     c(
       "usethis.quiet" = TRUE
