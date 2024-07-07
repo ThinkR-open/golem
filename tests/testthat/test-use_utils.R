2550f59fb8b22492d006bba313edf11358bf17fb
colin <colin@thinkr.fr>
2024-07-07 17:30:11 +0200

    test: use-utils test is now quiet

--- tests/testthat/test-use_utils.R
+++ tests/testthat/test-use_utils.R
@@ -1,27 +1,146 @@
-test_that("test use_utils_ui", {
-  with_dir(pkg, {
-    remove_file("R/golem_utils_ui.R")
-    use_utils_ui()
-    expect_exists("R/golem_utils_ui.R")
-    remove_file("R/golem_utils_ui.R")
+test_that("use_utils_ui works", {
+  on.exit(
+    unlink(pkg, TRUE, TRUE)
+  )
+  pkg <- file.path(
+    tempdir(),
+    "dummygolem"
+  )
 
-    remove_file("R/golem_utils_ui.R")
-    use_utils_ui(with_test = TRUE)
-    expect_exists("R/golem_utils_ui.R")
-    expect_exists("tests/testthat/test-golem_utils_ui.R")
+
+  dir.create(
+    file.path(
+      pkg,
+      "R"
+    ),
+    recursive = TRUE
+  )
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_ui(pkg = pkg)
+    }
+  )
+
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_ui.R"
+    )
+  )
+  remove_file(
+    file.path(
+      pkg,
+      "R/golem_utils_ui.R"
+    )
+  )
+  withr::with_dir(pkg, {
+    dir.create(
+      "tests/testthat/",
+      recursive = TRUE
+    )
   })
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_ui(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
+
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_ui.R"
+    )
+  )
+  expect_exists(
+    file.path(
+      pkg,
+      "tests/testthat/test-golem_utils_ui.R"
+    )
+  )
+  unlink(
+    pkg,
+    TRUE,
+    TRUE
+  )
 })
 
-test_that("test use_utils_server", {
-  with_dir(pkg, {
-    remove_file("R/golem_utils_server.R")
-    use_utils_server()
-    expect_exists("R/golem_utils_server.R")
-    remove_file("R/golem_utils_server.R")
-
-    remove_file("R/golem_utils_server.R")
-    use_utils_server(with_test = TRUE)
-    expect_exists("R/golem_utils_server.R")
-    expect_exists("tests/testthat/test-golem_utils_server.R")
+test_that("use_utils_ui works", {
+  on.exit(
+    unlink(pkg, TRUE, TRUE)
+  )
+  pkg <- file.path(
+    tempdir(),
+    "dummygolem"
+  )
+  dir.create(
+    file.path(
+      pkg,
+      "R"
+    ),
+    recursive = TRUE
+  )
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_server(pkg = pkg)
+    }
+  )
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_server.R"
+    )
+  )
+  remove_file(
+    file.path(
+      pkg,
+      "R/golem_utils_server.R"
+    )
+  )
+  withr::with_dir(pkg, {
+    dir.create(
+      "tests/testthat/",
+      recursive = TRUE
+    )
   })
+  withr::with_options(
+    c(
+      "usethis.quiet" = TRUE
+    ),
+    {
+      use_utils_server(
+        pkg = pkg,
+        with_test = TRUE
+      )
+    }
+  )
+
+  expect_exists(
+    file.path(
+      pkg,
+      "R/golem_utils_server.R"
+    )
+  )
+  expect_exists(
+    file.path(
+      pkg,
+      "tests/testthat/test-golem_utils_server.R"
+    )
+  )
+  unlink(
+    pkg,
+    TRUE,
+    TRUE
+  )
 })
