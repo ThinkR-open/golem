086897ebdc317a6edec066def7ff4a8e25b162db
colin <colin@thinkr.fr>
2024-07-07 19:14:47 +0200

    test: renamed test-reload

--- tests/testthat/test-zzzzzzzzzz.R
+++
@@ -1,7 +0,0 @@
-# # For setting back old usethis settings
-try({
-  unlink("README.Rmd", TRUE, TRUE)
-  unlink("README.md", TRUE, TRUE)
-  unlink(pkg, TRUE, TRUE)
-  options("usethis.quiet" = old_usethis.quiet)
-})
