6eeb7739f6f6087c197d8f297dac2da26047bef4
colin <colin@thinkr.fr>
2024-07-07 12:50:12 +0200

    test: document_and_reload tests

--- tests/testthat/test-zreload.R
+++ tests/testthat/test-zreload.R
@@ -1,28 +1,16 @@
-## fake package
-fakename <- paste0(sample(letters, 10, TRUE), collapse = "")
-tpdir <- tempdir()
-
-if (!dir.exists(file.path(tpdir, fakename))) {
-  create_golem(file.path(tpdir, fakename), open = FALSE)
-}
-pkg_reload <- file.path(tpdir, fakename)
-
 test_that("test document_and_reload", {
-  with_dir(pkg_reload, {
+  res <- perform_inside_a_new_golem(function() {
     file.create("R/sum.R")
     cat(
       "#' @export
     sum_golem <- function(a,b){a + b}",
       file = "R/sum.R"
     )
-    document_and_reload(pkg = ".")
-    testthat::expect_equal(sum_golem(1, 2), 3)
+    document_and_reload()
+    sum_golem(1, 2)
   })
+  testthat::expect_equal(
+    res,
+    3
+  )
 })
-
-# test_that("test detach_all_attached", {
-#   with_dir(pkg_reload, {
-#     test <- detach_all_attached()
-#     testthat::expect_true(test)
-#   })
-# })
