9a04c82a865e00344ad18d727b9650f3c6a89db5
colin <colin@thinkr.fr>
2024-07-07 20:50:30 +0200

    test: is_golem tests

--- tests/testthat/test-is_golem.R
+++ tests/testthat/test-is_golem.R
@@ -1,7 +1,30 @@
 test_that("is_golem works", {
-  expect_true(
-    is_golem(pkg)
+  dummy_golem <- create_dummy_golem()
+  to_create <- grep(
+    "^(?!REMOVEME).*",
+    list.files(
+      system.file("shinyexample", package = "golem"),
+      recursive = TRUE
+    ),
+    perl = TRUE,
+    value = TRUE
   )
+  withr::with_dir(
+    dummy_golem,
+    {
+      for (file in to_create) {
+        file.create(
+          file
+        )
+      }
+    }
+  )
+  expect_true(
+    is_golem(
+      dummy_golem
+    )
+  )
+  unlink(dummy_golem, TRUE, TRUE)
   expect_false(
     is_golem(tempdir())
   )
