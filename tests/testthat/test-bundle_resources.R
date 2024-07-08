2b727bc0bf3cd1d32353dec9d624e0da7b787942
colin <colin@thinkr.fr>
2024-07-08 10:54:13 +0200

    test: bundle resources

--- tests/testthat/test-bundle_resources.R
+++ tests/testthat/test-bundle_resources.R
@@ -1,26 +1,51 @@
-test_that("bundle_resources works", {
-  withr::with_dir(pkg, {
-    remove_file("inst/app/www/bundle.css")
-    remove_file("inst/app/www/bundle.js")
-    golem::add_css_file("bundle", open = FALSE)
-    golem::add_js_file("bundle", open = FALSE)
-    res <- bundle_resources(
-      path = "inst/app/www",
-      app_title = fakename,
-      with_sparkles = TRUE
-    )
-    expect_equal(
-      length(res),
-      3
-    )
-    expect_true(
-      inherits(res[[1]], "html_dependency")
-    )
-    expect_true(
-      inherits(res[[2]], "shiny.tag")
-    )
-    expect_true(
-      inherits(res[[3]], "html_dependency")
+test_that("multiplication works", {
+  dummy_golem <- create_dummy_golem()
+  on.exit({
+    unlink(
+      dummy_golem,
+      TRUE,
+      TRUE
     )
   })
+  withr::with_dir(
+    dummy_golem,
+    {
+      withr::with_options(
+        c("usethis.quiet" = TRUE),
+        {
+          golem::add_css_file(
+            pkg = ".",
+            "bundle",
+            open = FALSE
+          )
+          golem::add_js_file(
+            pkg = ".",
+            "bundle",
+            open = FALSE
+          )
+        }
+      )
+    }
+  )
+  res <- bundle_resources(
+    path = file.path(
+      dummy_golem,
+      "inst/app/www"
+    ),
+    app_title = "fakename",
+    with_sparkles = TRUE
+  )
+  expect_equal(
+    length(res),
+    3
+  )
+  expect_true(
+    inherits(res[[1]], "html_dependency")
+  )
+  expect_true(
+    inherits(res[[2]], "shiny.tag")
+  )
+  expect_true(
+    inherits(res[[3]], "html_dependency")
+  )
 })
