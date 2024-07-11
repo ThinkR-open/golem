bb4303366eda8fbd83c8158ed2dc85020673912c
colin <colin@thinkr.fr>
2024-07-11 09:42:52 +0200

    test: install_dev_deps tests

--- tests/testthat/test-install_dev_deps.R
+++ tests/testthat/test-install_dev_deps.R
@@ -1,32 +1,35 @@
 test_that("install_dev_deps works", {
-  install_dev_deps(
-    force_install = TRUE,
-    repos = "https://cran.rstudio.com"
-  )
-  paks <- unique(
-    c(
-      "usethis",
-      "pkgload",
-      "dockerfiler",
-      "devtools",
-      "roxygen2",
-      "attachment",
-      "rstudioapi",
-      "here",
-      "fs",
-      "desc",
-      "pkgbuild",
-      "processx",
-      "rsconnect",
-      "testthat",
-      "rstudioapi"
+  withr::with_temp_libpaths({
+    install_dev_deps(
+      force_install = TRUE,
+      repos = "https://cran.rstudio.com"
     )
-  )
-  for (
-    pak in paks
-  ) {
-    expect_true(
-      rlang::is_installed(pak)
+
+    paks <- unique(
+      c(
+        "usethis",
+        "pkgload",
+        "dockerfiler",
+        "devtools",
+        "roxygen2",
+        "attachment",
+        "rstudioapi",
+        "here",
+        "fs",
+        "desc",
+        "pkgbuild",
+        "processx",
+        "rsconnect",
+        "testthat",
+        "rstudioapi"
+      )
     )
-  }
+    for (
+      pak in paks
+    ) {
+      expect_true(
+        rlang::is_installed(pak)
+      )
+    }
+  })
 })
