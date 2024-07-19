0fbe4d4364de5b8c9914a525728c198ada2ef83c
Arthur Bréant <arthur@thinkr.fr>
2024-07-19 17:22:53 +0200

    feat: detect missing NS

---
+++ R/find_missing_ns.R
@@ -0,0 +1,171 @@
+#' @noRd
+stop_quietly <- function() {
+  opt <- options(show.error.messages = FALSE)
+  on.exit(options(opt))
+  stop()
+}
+
+#' @noRd
+is_shiny_input_output_funmodule <- function(
+  text,
+  extend_input_output_funmodule = NA_character_
+) {
+  stopifnot(is.character(extend_input_output_funmodule))
+
+  input_output_knew <- c("Input|Output|actionButton|radioButtons")
+  ui_funmodule_pattern <- c("mod_\\w+_ui")
+
+  patterns <- paste(
+    input_output_knew,
+    ui_funmodule_pattern,
+    sep = "|"
+  )
+
+  if (
+    !is.null(extend_input_output_funmodule) ||
+      !is.na(extend_input_output_funmodule) ||
+      extend_input_output_funmodule == ""
+  ) {
+    patterns <- paste(
+      patterns,
+      extend_input_output_funmodule,
+      sep = "|"
+    )
+  }
+
+  grepl(
+    pattern = patterns,
+    x = text
+  )
+}
+
+#' @noRd
+#' @importFrom utils getParseData
+check_namespace_in_file <- function(
+  path,
+  extend_input_output_funmodule = NA_character_
+) {
+  getParseData(
+    parse(
+      file = path,
+      keep.source = TRUE
+    )
+  ) |>
+    dplyr::mutate(
+      path = path
+    ) |>
+    dplyr::filter(
+      token == "SYMBOL_FUNCTION_CALL"
+    ) |>
+    dplyr::mutate(
+      is_input_output_funmodule = is_shiny_input_output_funmodule(
+        text = text,
+        extend_input_output_funmodule = extend_input_output_funmodule
+      )
+    ) |>
+    dplyr::mutate(
+      is_followed_by = dplyr::lead(text),
+      is_followed_by_ns = is_followed_by == "ns"
+    ) |>
+    dplyr::filter(
+      is_input_output_funmodule
+    )
+}
+
+#' @export
+check_namespace_sanity <- function(
+  pkg = golem::get_golem_wd(),
+  extend_input_output_funmodule = NA_character_,
+  disable = FALSE
+) {
+  if (disable) {
+    return(invisible(FALSE))
+  }
+
+  base_path <- normalizePath(
+    path = pkg,
+    mustWork = TRUE
+  )
+
+  if (!requireNamespace("desc", quietly = TRUE)) {
+    check_desc_installed()
+  }
+
+  encoding <- desc::desc_get("Encoding", file = base_path)[[1]]
+
+  if (!identical(encoding, "UTF-8")) {
+    warning("roxygen2 requires Encoding: UTF-8", call. = FALSE)
+  }
+
+  blocks <- roxygen2::parse_package(
+    path = ".",
+    env = NULL
+  )
+
+  shinymodule_blocks <- blocks |>
+    purrr::map(
+      .f = \(x) {
+        return <- roxygen2::block_get_tag(x, tag = "shinyModule")
+        if (is.null(return)) {
+          NULL
+        } else {
+          return
+        }
+      }
+    ) |>
+    purrr::compact()
+
+  if (length(shinymodule_blocks) == 0) {
+    cli::cli_alert_info("ok")
+    return(invisible(FALSE))
+  }
+
+  shinymodule_links <- shinymodule_blocks |>
+    purrr::map_chr(
+      .f = ~ .x[["file"]]
+    ) |>
+    unique()
+
+  data <- shinymodule_links |>
+    purrr::map_df(
+      .f = ~ check_namespace_in_file(
+        path = .x,
+        extend_input_output_funmodule = extend_input_output_funmodule
+      )
+    ) |>
+    dplyr::filter(
+      !is_followed_by_ns
+    ) |>
+    dplyr::select(
+      path,
+      text,
+      line1,
+      col1,
+      is_followed_by,
+      is_followed_by_ns
+    ) |>
+    dplyr::mutate(
+      message = sprintf("... see line %d in {.file %s:%d:%d}.", line1, path, line1, col1)
+    )
+
+  missing_ns_detected <- nrow(data)
+
+  cli::cli_text(
+    "It seems that ",
+    cli::bg_br_yellow(
+      "{missing_ns_detected} namespace{?s} (NS) {?is/are} missing..."
+    )
+  )
+
+  cli::cli_alert_info("Fix {?this/these} {missing_ns_detected} missing namespace{?s} before to continue...")
+
+  purrr::walk(data$message, cli::cli_alert_danger)
+
+  launch_app <- yesno::yesno("Is it fixed? Do you want to launch the app?")
+
+  if (isFALSE(launch_app)) {
+    stop_quietly()
+  }
+
+  return(TRUE)
+}
