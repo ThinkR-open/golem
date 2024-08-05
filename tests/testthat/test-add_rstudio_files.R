5fc7a61dafeab5c1c575c3af1dcb70f26c24e21f
colin <colin@thinkr.fr>
2024-08-05 21:29:13 +0200

    test: testing add_dockerfile

--- tests/testthat/test-add_rstudio_files.R
+++ tests/testthat/test-add_rstudio_files.R
@@ -1,34 +1,48 @@
 test_that("add_rstudio_files", {
-    for (fun in list(
-      add_positconnect_file,
-      add_shinyappsio_file,
-      add_shinyserver_file
-    )) {
-      dummy_golem <- create_dummy_golem()
-      withr::with_options(
-        c("usethis.quiet" = TRUE),
-        {
-          withr::with_dir(
-            dummy_golem,
-            {
-              fun(
-                pkg = dummy_golem,
-                open = FALSE
-              )
-            }
-          )
-        }
-      )
-      expect_exists(
-        file.path(
+  for (fun in list(
+    add_positconnect_file,
+    add_shinyappsio_file,
+    add_shinyserver_file
+  )) {
+    dummy_golem <- create_dummy_golem()
+    withr::with_options(
+      c("usethis.quiet" = TRUE),
+      {
+        withr::with_dir(
           dummy_golem,
-          "app.R"
+          {
+            fun(
+              pkg = dummy_golem,
+              open = FALSE
+            )
+          }
+        )
+      }
+    )
+    expect_exists(
+      file.path(
+        dummy_golem,
+        "app.R"
+      )
+    )
+    expect_true(
+      grepl(
+        "run_app",
+        paste(
+          readLines(
+            file.path(
+              dummy_golem,
+              "app.R"
+            )
+          ),
+          collapse = " "
         )
       )
-      unlink(
-        dummy_golem,
-        TRUE,
-        TRUE
-      )
-    }
+    )
+    unlink(
+      dummy_golem,
+      TRUE,
+      TRUE
+    )
+  }
 })
