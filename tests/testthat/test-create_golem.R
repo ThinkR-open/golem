70c594678b81c7fda222b2c4b5c311f7b2f4c7c8
colin <colin@thinkr.fr>
2024-08-07 07:01:00 +0200

    test: testing create golem

--- tests/testthat/test-create_golem.R
+++ tests/testthat/test-create_golem.R
@@ -1,31 +1,54 @@
-test_that("create_golem works", {
-  res <- perform_inside_a_new_golem(function() {
-    return(getwd())
-  })
-  expect_exists(
-    res
+is_properly_populated_golem <- function(path) {
+  # All files excepts *.Rproj which changes based on the project name
+  expected_files <- list.files(
+    golem_sys(
+      "shinyexample"
+    ),
+    recursive = TRUE
   )
-  expect_exists(
-    file.path(
-      res,
-      "DESCRIPTION"
-    )
-  )
-  expect_exists(
-    file.path(
-      res,
-      "R"
-    )
-  )
+  expected_files <- expected_files[!grepl(
+    "Rproj",
+    expected_files
+  )]
 
-  expect_exists(
-    file.path(
-      res,
-      "inst"
-    )
+  expected_files <- expected_files[!grepl(
+    "REMOVEME.Rbuildignore",
+    expected_files
+  )]
+
+  actual_files <- list.files(path, recursive = TRUE)
+
+  identical(
+    sort(expected_files),
+    sort(actual_files)
+  )
+}
+
+test_that("create_golem works", {
+  testthat::with_mocked_bindings(
+    usethis_create_project = function(path, open){
+      dir.create(path, recursive = TRUE)
+    },
+    here_set_here = function(path){
+      return(TRUE)
+    },{
+      dir <- tempfile(pattern = "golemcreategolem")
+      withr::with_options(
+        c("usethis.quiet" = TRUE),{
+          dir <- create_golem(
+            dir,
+            open = FALSE,
+            package_name = "testpkg"
+          )
+        }
+      )
+      expect_true(is_properly_populated_golem(
+        dir
+      ))
+    }
   )
   unlink(
-    res,
+    dir,
     TRUE,
     TRUE
   )
