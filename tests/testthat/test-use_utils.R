a0034d3f411caaa40ba2e2a04dc61cebdf1a4b4a
colin <colin@thinkr.fr>
2024-08-16 14:31:28 +0200

    test: use_utils in new golem

--- tests/testthat/test-use_utils.R
+++ tests/testthat/test-use_utils.R
@@ -1,199 +1,129 @@
 test_that("use_utils_ui works", {
-  on.exit(
-    unlink(pkg, TRUE, TRUE)
-  )
-  pkg <- create_dummy_golem()
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_ui(pkg = pkg)
-    }
-  )
+  run_quietly_in_a_dummy_golem({
+    use_utils_ui(".")
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_ui.R"
+      )
+    )
+    remove_file(
+      file.path(
+        ".",
+        "R/golem_utils_ui.R"
+      )
+    )
 
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_ui.R"
+    use_utils_ui(
+      ".",
+      with_test = TRUE
     )
-  )
-  remove_file(
-    file.path(
-      pkg,
-      "R/golem_utils_ui.R"
+    # Testint that the function is
+    # indempotent by running it twice
+    use_utils_ui(
+      ".",
+      with_test = TRUE
     )
-  )
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_ui(
-        pkg = pkg,
-        with_test = TRUE
-      )
-    }
-  )
-  # Testint that the function is
-  # indempotent by running it twice
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_ui(
-        pkg = pkg,
-        with_test = TRUE
-      )
-    }
-  )
 
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_ui.R"
-    )
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "tests/testthat/test-golem_utils_ui.R"
-    )
-  )
-  # We test that we can still add the tests even if the utils ui
-  # is already there
-  remove_file(
-    file.path(
-      pkg,
-      "R/golem_utils_ui.R"
-    )
-  )
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_ui(
-        pkg = pkg,
-        with_test = TRUE
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_ui.R"
       )
-    }
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_ui.R"
     )
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "tests/testthat/test-golem_utils_ui.R"
+    expect_exists(
+      file.path(
+        ".",
+        "tests/testthat/test-golem_utils_ui.R"
+      )
     )
-  )
-  unlink(
-    pkg,
-    TRUE,
-    TRUE
-  )
+
+    # We test that we can still add the tests even if the utils ui
+    # is already there
+    remove_file(
+      file.path(
+        ".",
+        "R/golem_utils_ui.R"
+      )
+    )
+    use_utils_ui(
+      ".",
+      with_test = TRUE
+    )
+
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_ui.R"
+      )
+    )
+    expect_exists(
+      file.path(
+        ".",
+        "tests/testthat/test-golem_utils_ui.R"
+      )
+    )
+  })
 })
 
-test_that("use_utils_ui works", {
-  on.exit(
-    unlink(pkg, TRUE, TRUE)
-  )
-  pkg <- create_dummy_golem()
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_server(pkg = pkg)
-    }
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_server.R"
-    )
-  )
-  remove_file(
-    file.path(
-      pkg,
-      "R/golem_utils_server.R"
-    )
-  )
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_server(
-        pkg = pkg,
-        with_test = TRUE
+test_that("use_utils_server works", {
+  run_quietly_in_a_dummy_golem({
+    use_utils_server(".")
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_server.R"
       )
-    }
-  )
-  # Testint that the function is
-  # indempotent by running it twice
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_server(
-        pkg = pkg,
-        with_test = TRUE
+    )
+    remove_file(
+      file.path(
+        ".",
+        "R/golem_utils_server.R"
       )
-    }
-  )
+    )
+    use_utils_server(
+      ".",
+      with_test = TRUE
+    )
+    # Testing the fun is indempotent
+    use_utils_server(
+      ".",
+      with_test = TRUE
+    )
 
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_server.R"
-    )
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "tests/testthat/test-golem_utils_server.R"
-    )
-  )
-  remove_file(
-    file.path(
-      pkg,
-      "R/golem_utils_server.R"
-    )
-  )
-  withr::with_options(
-    c(
-      "usethis.quiet" = TRUE
-    ),
-    {
-      use_utils_server(
-        pkg = pkg,
-        with_test = TRUE
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_server.R"
       )
-    }
-  )
-
-  expect_exists(
-    file.path(
-      pkg,
-      "R/golem_utils_server.R"
     )
-  )
-  expect_exists(
-    file.path(
-      pkg,
-      "tests/testthat/test-golem_utils_server.R"
+    expect_exists(
+      file.path(
+        ".",
+        "tests/testthat/test-golem_utils_server.R"
+      )
     )
-  )
-  unlink(
-    pkg,
-    TRUE,
-    TRUE
-  )
+    remove_file(
+      file.path(
+        ".",
+        "R/golem_utils_server.R"
+      )
+    )
+    use_utils_server(
+      ".",
+      with_test = TRUE
+    )
+    expect_exists(
+      file.path(
+        ".",
+        "R/golem_utils_server.R"
+      )
+    )
+    expect_exists(
+      file.path(
+        ".",
+        "tests/testthat/test-golem_utils_server.R"
+      )
+    )
+  })
 })
