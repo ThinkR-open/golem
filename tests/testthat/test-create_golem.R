7c6100ed621a6f32657527091ddfabf56f0ff867
colin <colin@thinkr.fr>
2024-09-06 19:06:32 +0200

    test: testing golem creation + fixing removecomment issue

--- tests/testthat/test-create_golem.R
+++ tests/testthat/test-create_golem.R
@@ -23,8 +23,8 @@ is_properly_populated_golem <- function(path) {
     sort(actual_files)
   )
 }
-
-test_that("create_golem works", {{ dir <- tempfile(pattern = "golemcreategolem")
+test_that("create_golem works", {
+  dir <- tempfile(pattern = "golemcreategolem")
   withr::with_options(
     c("usethis.quiet" = TRUE),
     {
@@ -44,7 +44,104 @@ test_that("create_golem works", {{ dir <- tempfile(pattern = "golemcreategolem")
     dir,
     TRUE,
     TRUE
-  ) }})
+  )
+})
+
+test_that("create_golem fails if the dir already exists", {
+  dir <- tempfile(pattern = "golemcreategolemfail")
+  dir.create(dir)
+  expect_error({
+    withr::with_options(
+      c("usethis.quiet" = TRUE),
+      {
+        dir <- create_golem(
+          dir,
+          open = FALSE,
+          package_name = "testpkg"
+        )
+      }
+    )
+  })
+
+  unlink(
+    dir,
+    TRUE,
+    TRUE
+  )
+})
+
+test_that("create_golem override if the dir already exists and overwrite is set to TRUE", {
+  dir <- tempfile(pattern = "golemcreategolemfail")
+  dir.create(dir)
+  withr::with_options(
+    c("usethis.quiet" = TRUE),
+    {
+      dir <- create_golem(
+        dir,
+        overwrite = TRUE,
+        open = FALSE,
+        package_name = "testpkg"
+      )
+    }
+  )
+  expect_true(
+    is_properly_populated_golem(
+      dir
+    )
+  )
+  unlink(
+    dir,
+    TRUE,
+    TRUE
+  )
+})
+
+test_that("create_golem can remove comments", {
+  dir <- tempfile(pattern = "golemcreategolemfail")
+
+  withr::with_options(
+    c("usethis.quiet" = TRUE),
+    {
+      dir <- create_golem(
+        dir,
+        without_comments = TRUE,
+        open = FALSE,
+        package_name = "testpkg"
+      )
+    }
+  )
+  expect_equal(
+    length(
+      readLines(
+        file.path(
+          dir,
+          "dev",
+          "01_start.R"
+        )
+      )
+    ),
+    31
+  )
+  expect_false(
+    any(
+      grepl(
+        "^# *",
+        readLines(
+          file.path(
+            dir,
+            "dev",
+            "01_start.R"
+          )
+        )
+      )
+    )
+  )
+  unlink(
+    dir,
+    TRUE,
+    TRUE
+  )
+})
 
 test_that("create_golem_gui works", {
   testthat::with_mocked_bindings(
