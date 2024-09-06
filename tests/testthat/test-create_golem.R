544a7b7a9a32d40452ffa8127682e8a1faafd83f
colin <colin@thinkr.fr>
2024-09-06 18:15:24 +0200

    test: now using create_golem in tests

--- tests/testthat/test-create_golem.R
+++ tests/testthat/test-create_golem.R
@@ -24,33 +24,27 @@ is_properly_populated_golem <- function(path) {
   )
 }
 
-test_that("create_golem works", {
-  testthat::with_mocked_bindings(
+test_that("create_golem works", {{ dir <- tempfile(pattern = "golemcreategolem")
+  withr::with_options(
+    c("usethis.quiet" = TRUE),
     {
-      dir <- tempfile(pattern = "golemcreategolem")
-      withr::with_options(
-        c("usethis.quiet" = TRUE),
-        {
-          dir <- create_golem(
-            dir,
-            open = FALSE,
-            package_name = "testpkg"
-          )
-        }
-      )
-      expect_true(
-        is_properly_populated_golem(
-          dir
-        )
+      dir <- create_golem(
+        dir,
+        open = FALSE,
+        package_name = "testpkg"
       )
     }
+  )
+  expect_true(
+    is_properly_populated_golem(
+      dir
+    )
   )
   unlink(
     dir,
     TRUE,
     TRUE
-  )
-})
+  ) }})
 
 test_that("create_golem_gui works", {
   testthat::with_mocked_bindings(
