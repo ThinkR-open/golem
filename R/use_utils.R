41a61b1ccafea6fb85dbef508eed722cc79f343c
colin <colin@thinkr.fr>
2025-05-22 12:27:38 +0200

    feat: golem_wd in use_utils

--- R/use_utils.R
+++ R/use_utils.R
@@ -14,13 +14,19 @@
 #'
 #' @return Used for side-effects.
 use_utils_ui <- function(
-  pkg = get_golem_wd(),
-  with_test = FALSE
+  golem_wd = get_golem_wd(),
+  with_test = FALSE,
+  pkg
 ) {
+  signal_arg_is_deprecated(
+    pkg,
+    fun = as.character(sys.call()[[1]]),
+    "pkg"
+  )
   added <- use_utils(
     file_name = "golem_utils_ui.R",
     folder_name = "R",
-    pkg = pkg
+    golem_wd = golem_wd
   )
 
   if (added) {
@@ -31,20 +37,20 @@ use_utils_ui <- function(
         usethis_use_testthat()
       }
       pth <- fs_path(
-        pkg,
+        golem_wd,
         "tests",
         "testthat",
         "test-golem_utils_ui.R"
       )
-      if (
-        file.exists(pth)
-      ) {
+      if (file.exists(pth)) {
         file_already_there_dance(
           where = pth,
           open_file = FALSE
         )
       } else {
-        use_utils_test_ui(pkg = pkg)
+        use_utils_test_ui(
+          golem_wd = golem_wd
+        )
       }
     }
   }
@@ -52,11 +58,19 @@ use_utils_ui <- function(
 
 #' @export
 #' @rdname utils_files
-use_utils_test_ui <- function(pkg = get_golem_wd()) {
+use_utils_test_ui <- function(
+  golem_wd = get_golem_wd(),
+  pkg
+) {
+  signal_arg_is_deprecated(
+    pkg,
+    fun = as.character(sys.call()[[1]]),
+    "pkg"
+  )
   added <- use_utils(
     file_name = "test-golem_utils_ui.R",
     folder_name = "tests/testthat",
-    pkg = pkg
+    golem_wd = golem_wd
   )
 
   if (added) {
@@ -67,13 +81,19 @@ use_utils_test_ui <- function(pkg = get_golem_wd()) {
 #' @export
 #' @rdname utils_files
 use_utils_server <- function(
-  pkg = get_golem_wd(),
-  with_test = FALSE
+  golem_wd = get_golem_wd(),
+  with_test = FALSE,
+  pkg
 ) {
+  signal_arg_is_deprecated(
+    pkg,
+    fun = as.character(sys.call()[[1]]),
+    "pkg"
+  )
   added <- use_utils(
     file_name = "golem_utils_server.R",
     folder_name = "R",
-    pkg = pkg
+    golem_wd = golem_wd
   )
   if (added) {
     cat_green_tick("Utils server added")
@@ -83,21 +103,19 @@ use_utils_server <- function(
         usethis_use_testthat()
       }
       pth <- fs_path(
-        pkg,
+        golem_wd,
         "tests",
         "testthat",
         "test-golem_utils_server.R"
       )
-      if (
-        file.exists(pth)
-      ) {
+      if (file.exists(pth)) {
         file_already_there_dance(
           where = pth,
           open_file = FALSE
         )
       } else {
         use_utils_test_server(
-          pkg = pkg
+          golem_wd = golem_wd
         )
       }
     }
@@ -105,14 +123,14 @@ use_utils_server <- function(
 }
 
 use_utils_test_ <- function(
-  pkg = get_golem_wd(),
+  golem_wd = get_golem_wd(),
   type = c("ui", "server")
 ) {
   type <- match.arg(type)
   added <- use_utils(
     file_name = sprintf("test-golem_utils_%s.R", type),
     folder_name = "tests/testthat",
-    pkg = pkg
+    golem_wd = golem_wd
   )
 
   if (added) {
@@ -123,30 +141,48 @@ use_utils_test_ <- function(
 #' @export
 #' @rdname utils_files
 use_utils_test_ui <- function(
-  pkg = get_golem_wd()) {
-  use_utils_test_(
+  golem_wd = get_golem_wd(),
+  pkg
+) {
+  signal_arg_is_deprecated(
     pkg,
+    fun = as.character(sys.call()[[1]]),
+    "pkg"
+  )
+  use_utils_test_(
+    golem_wd,
     "ui"
   )
 }
 #' @export
 #' @rdname utils_files
-use_utils_test_server <- function(pkg = get_golem_wd()) {
-  use_utils_test_(pkg, "server")
+use_utils_test_server <- function(
+  golem_wd = get_golem_wd(),
+  pkg
+) {
+  signal_arg_is_deprecated(
+    pkg,
+    fun = as.character(sys.call()[[1]]),
+    "pkg"
+  )
+  use_utils_test_(
+    golem_wd,
+    "server"
+  )
 }
 
 use_utils <- function(
   file_name,
   folder_name,
-  pkg = get_golem_wd()
+  golem_wd = get_golem_wd()
 ) {
   old <- setwd(
-    fs_path_abs(pkg)
+    fs_path_abs(golem_wd)
   )
   on.exit(setwd(old))
 
   destination <- fs_path(
-    fs_path_abs(pkg),
+    fs_path_abs(golem_wd),
     folder_name,
     file_name
   )
