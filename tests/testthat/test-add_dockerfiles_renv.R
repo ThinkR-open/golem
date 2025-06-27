292d10652c5f73dfea54ebb5666cda27f40e77cb
vincent guyader <vincent@thinkr.fr>
2025-06-27 07:39:21 +0200

    Update tests/testthat/test-add_dockerfiles_renv.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-add_dockerfiles_renv.R
+++ tests/testthat/test-add_dockerfiles_renv.R
@@ -1,30 +1,30 @@
 test_that("add_dockerfile_with_renv_ works", {
-  skip_on_cran()
-  skip_if_not_installed("renv")
-  skip_if_not_installed("dockerfiler", "0.2.3")
-  skip_if_not_installed("attachment", "0.2.5")
-  dummy_golem <- create_dummy_golem()
-  testthat::with_mocked_bindings(
-    attachment_create_renv_for_prod = function(...) {
-      return(normalizePath("renv.lock.prod"))
-    },
-    dockerfiler_dock_from_renv = function(...) {
-      return(
-        readRDS(
-          "dockerfiler_dock_from_renv_socle.RDS"
-        )
-      )
-    },
-    {
-      withr::with_options(
-        c("usethis.quiet" = TRUE),
-        {
-          dockerfile_with_renv <- add_dockerfile_with_renv_(
-            golem_wd = dummy_golem,
-            lockfile = "renv.lock.prod",
-            pkgbuild_quiet = TRUE,
-            output_dir=tempfile()
-          )
+	skip_on_cran()
+	skip_if_not_installed("renv")
+	skip_if_not_installed("dockerfiler", "0.2.3")
+	skip_if_not_installed("attachment", "0.2.5")
+	dummy_golem <- create_dummy_golem()
+	testthat::with_mocked_bindings(
+		attachment_create_renv_for_prod = function(...) {
+			return(normalizePath("renv.lock.prod"))
+		},
+		dockerfiler_dock_from_renv = function(...) {
+			return(
+				readRDS(
+					"dockerfiler_dock_from_renv_socle.RDS"
+				)
+			)
+		},
+		{
+			withr::with_options(
+				c("usethis.quiet" = TRUE),
+				{
+					dockerfile_with_renv <- add_dockerfile_with_renv_(
+						golem_wd = dummy_golem,
+						lockfile = "renv.lock.prod",
+						pkgbuild_quiet = TRUE,
+						output_dir = tempfile()
+					)
 
           dockerfile_with_renv_output <- add_dockerfile_with_renv(
             golem_wd = dummy_golem,
