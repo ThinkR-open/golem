b3cbc5969dfbad0e2cb2c8d7b6814c8e1447515c
Carson <cpsievert1@gmail.com>
2025-06-30 11:40:52 -0500

    Update tests to not make strong assumptions about actionButton()'s markup

--- inst/utils/test-golem_utils_ui.R
+++ inst/utils/test-golem_utils_ui.R
@@ -167,21 +167,12 @@ test_that("Test undisplay works", {
 		"go_filter",
 		"go"
 	)
-	expect_s3_class(
-		b,
-		"shiny.tag"
-	)
 	expect_equal(
-		as.character(b),
-		'<button id="go_filter" type="button" class="btn btn-default action-button">go</button>'
-	)
-	b_undisplay <- undisplay(b)
-	expect_s3_class(b, "shiny.tag")
-	expect_equal(
-		as.character(
-			b_undisplay
+		htmltools::tagGetAttribute(
+			undisplay(b),
+			"style"
 		),
-		'<button id="go_filter" type="button" class="btn btn-default action-button" style="display: none;">go</button>'
+		"display: none;"
 	)
 
 	c <- shiny::tags$p(
