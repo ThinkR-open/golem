5bfae5b9f679dca39847cce0975d192c8334db34
IlyaZar <zarubin@wiso.uni-koeln.de>
2026-04-20 10:27:59 +0200

    test: cover empty module fct/utils suffixes

--- tests/testthat/test-add_r_files.R
+++ tests/testthat/test-add_r_files.R
@@ -236,3 +236,43 @@ test_that("add_fct sanitizes names correctly", {
 		)
 	})
 })
+
+test_that("add_module with empty fct or utils does not create trailing underscore filenames", {
+	run_quietly_in_a_dummy_golem({
+		add_module(
+			"FixHumanity",
+			fct = "",
+			open = FALSE
+		)
+		expect_exists(
+			file.path(
+				"R",
+				"mod_FixHumanity_fct.R"
+			)
+		)
+		expect_false(
+			file.exists(file.path(
+				"R",
+				"mod_FixHumanity_fct_.R"
+			))
+		)
+
+		add_module(
+			"FixCompassion",
+			utils = "",
+			open = FALSE
+		)
+		expect_exists(
+			file.path(
+				"R",
+				"mod_FixCompassion_utils.R"
+			)
+		)
+		expect_false(
+			file.exists(file.path(
+				"R",
+				"mod_FixCompassion_utils_.R"
+			))
+		)
+	})
+})
