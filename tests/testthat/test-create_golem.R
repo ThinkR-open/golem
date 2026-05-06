3c8a3fa36db3e6a9fbdfe1347b1f8152704e0eea
IlyaZar <zarubin@wiso.uni-koeln.de>
2026-05-07 00:03:58 +0200

    tests: add tests for previous two commits

--- tests/testthat/test-create_golem.R
+++ tests/testthat/test-create_golem.R
@@ -433,6 +433,58 @@ test_that("create_golem with git works", {
 	)
 })
 
+test_that("check_arg_with_agents_options validates option names", {
+	expect_null(check_arg_with_agents_options(NULL))
+	expect_null(check_arg_with_agents_options(list()))
+	expect_null(check_arg_with_agents_options(list(skills = "all")))
+
+	expect_error(
+		check_arg_with_agents_options("all"),
+		"`with_agents_options` must be `NULL` or a named list",
+		fixed = TRUE
+	)
+	expect_error(
+		check_arg_with_agents_options(list("all")),
+		"`with_agents_options` must be a named list",
+		fixed = TRUE
+	)
+	expect_error(
+		check_arg_with_agents_options(
+			list(skills = "all", skills = "golem-run-tests")
+		),
+		"Duplicated names"
+	)
+	expect_error(
+		check_arg_with_agents_options(list(golem_wd = "/tmp/project")),
+		"Names in `with_agents_options` are wrong",
+		fixed = TRUE
+	)
+})
+
+test_that("create_golem_use_agents dispatches through use_skills", {
+	result <- testthat::with_mocked_bindings(
+		use_skills = function(...) {
+			list(...)
+		},
+		{
+			create_golem_use_agents(
+				path_to_golem = "/tmp/project",
+				with_agents_options = list(skills = "all"),
+				should_prompt = FALSE
+			)
+		}
+	)
+
+	expect_equal(
+		result,
+		list(
+			skills = "all",
+			golem_wd = "/tmp/project",
+			interactive = FALSE
+		)
+	)
+})
+
 test_that("create_golem_gui works", {
 	testthat::with_mocked_bindings(
 		create_golem = function(
